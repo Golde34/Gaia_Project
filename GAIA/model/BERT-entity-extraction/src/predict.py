9e6fa0397fc30a35429d9a22ec075c4e6eee88f5
Golde34 <nguyendongducviet2001@gmail.com>
2023-05-28 01:39:34 +0700

    finish output predict

--- GAIA/model/BERT-entity-extraction/src/predict.py
+++ GAIA/model/BERT-entity-extraction/src/predict.py
@@ -7,7 +7,6 @@ from bert_model import EntityModel
 
 
 def handle_input(sentence):
-
     # transfer the output tag and pos with tokenized sentence
     tokenized_sentence = config.TOKENIZER.encode(sentence)
     sentenceList = sentence.split()
@@ -16,7 +15,6 @@ def handle_input(sentence):
 
 
 def predict(token, sentenceList):
-
     test_dataset = predict_setup_dataset(sentenceList)
 
     meta_data = joblib.load("meta.bin")
@@ -41,9 +39,7 @@ def predict(token, sentenceList):
     res_tag = result_tag(tag, enc_tag, token)
     res_pos = result_pos(pos, enc_pos, token)
 
-    json_result = predict_output(res_tag, res_pos, token)
-    print(json_result)
-
+    return res_tag, res_pos, token
 
 def predict_setup_dataset(sentenceList):
 
@@ -72,15 +68,13 @@ def result_pos(pos, enc_pos, token):
 
 
 def predict_output(res_tag, res_pos, token):
-
     word_list = make_word_list(res_tag, res_pos, token)
-    json_result = return_list_predict_json(word_list)
+    word_json_result = filter_words(word_list)
+    json_result = return_phrase_json(word_json_result)
+
     return json_result
 
-
-
 def make_word_list(res_tag, res_pos, token):
-
     word_list = []  # encoded word list stored
 
     for s in range(1, len(token) - 1):  # Do not count the CLS and SEP
@@ -99,8 +93,13 @@ def make_word_list(res_tag, res_pos, token):
 
                 word_list.pop()  # delete the previous word which is not completed word
                 add_word(word_list, res_tag[s], res_pos[s], completed_word)  # add again
+
             else:
                 add_word(word_list, res_tag[s], res_pos[s], word)
+
+            # print(res_tag[s], decode(token[s]), res_pos[s])
+            # print(word_list)
+
     return word_list
 
 def decode(token):
@@ -114,7 +113,7 @@ def add_word(array, tag, pos, word):
     dictionary = {"tag": tag, "pos": pos, "word": word}
     array.append(dictionary)
 
-def return_list_predict_json(word_list):
+def filter_words(word_list):
     per_list = []  # person
     geo_list = []  # geographic entity
     org_list = []  # organization
@@ -125,22 +124,69 @@ def return_list_predict_json(word_list):
     nat_list = []  # natural phenomenon
 
     for word in word_list:
-        if word["tag"] == 'B-per' or word["tag"] == 'I-per':
-            per_list.append(word["word"])
-        if word["tag"] == 'B-geo' or word["tag"] == 'I-geo':
-            geo_list.append(word["word"])
-        if word["tag"] == 'B-gpe' or word["tag"] == 'I-gpe':
-            gpe_list.append(word["word"])
-        if word["tag"] == 'B-org' or word["tag"] == 'I-org':
-            org_list.append(word["word"])
-        if word["tag"] == 'B-tim' or word["tag"] == 'I-tim':
-            tim_list.append(word["word"])
-        if word["tag"] == 'B-art' or word["tag"] == 'I-art':
-            art_list.append(word["word"])
-        if word["tag"] == 'B-eve' or word["tag"] == 'I-eve':
-            eve_list.append(word["word"])
-        if word["tag"] == 'B-nat' or word["tag"] == 'I-nat':
-            nat_list.append(word["word"])
+        if word["tag"].__contains__('per'):
+            per_list.append(word)
+        elif word["tag"].__contains__('geo'):
+            geo_list.append(word)
+        elif word["tag"].__contains__('gpe'):
+            gpe_list.append(word)
+        elif word["tag"].__contains__('org'):
+            org_list.append(word)
+        elif word["tag"].__contains__('tim'):
+            tim_list.append(word)
+        elif word["tag"].__contains__('art'):
+            art_list.append(word)
+        elif word["tag"].__contains__('eve'):
+            eve_list.append(word)
+        elif word["tag"].__contains__('nat'):
+            nat_list.append(word)
+
+    word_json_result = {
+        "person": per_list,
+        "geographic": geo_list,
+        "organization": org_list,
+        "geopolitical": gpe_list,
+        "time_indicator": tim_list,
+        "artifact": art_list,
+        "event": eve_list,
+        "natural": nat_list
+    }
+
+    return word_json_result
+
+def return_phrase_json(word_json_result):
+
+    per_list = []  # person
+    geo_list = []  # geographic entity
+    org_list = []  # organization
+    gpe_list = []  # geopolitical entity
+    tim_list = []  # time indicator
+    art_list = []  # artifact
+    eve_list = []  # event
+    nat_list = []  # natural phenomenon
+
+    word_combinations = []
+    current_combination = []
+
+    for tag_list in word_json_result.values():
+        add_phrase(tag_list, word_combinations, current_combination)
+
+    for phrase in word_combinations:
+        if phrase['tag'].__contains__('per'):
+            per_list.append(phrase)
+        elif phrase['tag'].__contains__('geo'):
+            geo_list.append(phrase)
+        elif phrase['tag'].__contains__('gpe'):
+            gpe_list.append(phrase)
+        elif phrase['tag'].__contains__('org'):
+            org_list.append(phrase)
+        elif phrase['tag'].__contains__('tim'):
+            tim_list.append(phrase)
+        elif phrase['tag'].__contains__('eve'):
+            eve_list.append(phrase)
+        elif phrase['tag'].__contains__('nat'):
+            nat_list.append(phrase)
+
     json_result = {
         "person": per_list,
         "geographic": geo_list,
@@ -151,10 +197,43 @@ def return_list_predict_json(word_list):
         "event": eve_list,
         "natural": nat_list
     }
+
     return json_result
+
+def add_phrase(tag_list, word_combinations, current_combination):
+
+    for i in range(0, len(tag_list)):
+        tag = tag_list[i]['tag']
+        word = tag_list[i]['word']
+        if tag.startswith("B-"):
+            if i + 1 < len(tag_list):
+                if tag_list[i + 1]['tag'].startswith("B-"):
+                    temp_dict = {"tag": tag[2:], "word": word}
+                    word_combinations.append(temp_dict)
+                elif tag_list[i + 1]['tag'].startswith("I-"):
+                    current_combination.append(word)
+            else:
+                temp_dict = {"tag": tag[2:], "word": word}
+                word_combinations.append(temp_dict)
+
+        elif tag.startswith("I-"):
+            current_combination.append(word)
+            if i + 1 < len(tag_list):
+                if tag_list[i + 1]['tag'].startswith("B-"):
+                    temp_dict = {"tag": tag[2:], "word": " ".join(current_combination)}
+                    word_combinations.append(temp_dict)
+                    current_combination = []
+            else:
+                temp_dict = {"tag": tag[2:], "word": " ".join(current_combination)}
+                word_combinations.append(temp_dict)
+                current_combination = []
+
+
 # run the def main
 if __name__ == "__main__":
 
     ip = str(input())
     token_sentence, sentence_list = handle_input(ip)
-    predict(token_sentence, sentence_list)
+    _tag, _pos, _token = predict(token_sentence, sentence_list)
+    json_output = predict_output(_tag, _pos, _token)
+    print(json_output)
