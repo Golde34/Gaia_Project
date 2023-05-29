221ffd1475b5709935a5de7dc0b7d370d789c97e
Golde34 <nguyendongducviet2001@gmail.com>
2023-05-29 23:49:52 +0700

    create gaia bot
    create gaia bot

---
+++ GAIA/gaia_bot/main.py
@@ -0,0 +1,5 @@
+from core.console_manager import ConsoleManager
+def main():
+    # Startup
+    console_manager = ConsoleManager()
+    console_manager.console_output(info_log="Wait a second for startup check...")
