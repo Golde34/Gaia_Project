6103712329d48a30c2edf28131cb32232194ddde
Golde34 <82593925+Golde34@users.noreply.github.com>
2023-06-08 09:07:06 +0700

    delete base learn
    delete base learn

--- base learn/base/beta1/Dream/engines/ltl.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-
-translator = Translator()
-
-def L2L(text, from_l, to_l):
-    return translator.translate(text, src=from_l,dest=to_l).text
--- base learn/base/beta1/Dream/utils/sound.py
+++
@@ -1,8 +0,0 @@
-import os
-from playsound import playsound
-
-
-def play_activation_sound():
-    utils_dir = os.path.dirname(__file__)
-    activation_soundfile = os.path.join(utils_dir, '..', 'resources\\music', "activation_sound.wav")
-    playsound(activation_soundfile)
