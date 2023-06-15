4861418bd21eee40ae3917cc1262f86cd727e594
Golde34 <nguyendongducviet2001@gmail.com>
2023-06-15 20:27:07 +0700

    gpt4all remove

--- indi_model/gpt4all/utils/read.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-
-def read_config(path):
-    with open(path, 'r') as file:
-        try:
-            return yaml.safe_load(file)
-        except yaml.YAMLError as e:
-            print(e)
