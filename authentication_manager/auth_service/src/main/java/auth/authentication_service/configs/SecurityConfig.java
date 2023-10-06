6597c14b0cc8a20f87156fa9be817eb169bd5386
Golde34 <nguyendongducviet2001@gmail.com>
2023-10-06 16:09:49 +0700

    add cors security

--- authentication_manager/auth_service/src/main/java/auth/authentication_service/configs/SecurityConfig.java
+++ authentication_manager/auth_service/src/main/java/auth/authentication_service/configs/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(cors -> cors.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .securityContext((securityContext) -> securityContext.requireExplicitSave(true))
                 .authorizeHttpRequests(authz -> {
                     authz
