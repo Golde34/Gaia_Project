cc79ac58a9b7e58b122cd314e535a4690cfe2fad
Golde34 <nguyendongducviet2001@gmail.com>
2023-10-21 23:55:43 +0700

    finished authentication connection between gaia and auth service

--- GAIA/gaia_bot/modules/ports/commands/authen_command.py
+++ GAIA/gaia_bot/modules/ports/commands/authen_command.py
@@ -10,8 +10,8 @@ class AuthenticationConnector:
         self.username = username
         self.password = password
         self.gaia_port = PORTS['gaia_connector']['port']
-        self.auth_port = PORTS['authentication_service']['port']
         self.auth_bash = PORTS['authentication_service']['shell_path']
+        self.gaia_url = f"http://localhost:{self.gaia_port}"
     
     def activate_authentication_command(self):
         if (self._check_microservice_status() == False):
@@ -25,9 +25,7 @@ class AuthenticationConnector:
         username = USER_PROFILE['username']
         password = USER_PROFILE['password']
         
-        flask_url = f"http://localhost:{self.gaia_port}/auth"
-        
-        response = requests.post(f"{flask_url}", json={'username': username, 'password': password})
+        response = requests.post(f"{self.gaia_url}/auth/sign-in", json={'username': username, 'password': password})
         
         if response.status_code == 200:
             result = response.json()
@@ -46,9 +44,8 @@ class AuthenticationConnector:
             print("Error running the bash script: ", e)
 
     def _check_microservice_status(self):
-        flask_url = f"http://localhost:{self.gaia_port}/"
         
-        response = requests.get(f"{flask_url}/status")
+        response = requests.get(f"{self.gaia_url}/status")
         
         if response.status_code == 200:
             return True
--- GAIA/gaia_connector/controllers/auth_service_controller.py
+++ GAIA/gaia_connector/controllers/auth_service_controller.py
@@ -1,19 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 import requests
 from controllers import app
 from configs.port_configs import PORTS
 
 
 authentication_service_port = PORTS['authentication_service']
+authentication_service_url = f"http://localhost:{authentication_service_port['port']}"
 
-@app.route('/auth', methods=['POST'])
+@app.route('/auth/sign-in', methods=['POST'])
 def auth():
     data = request.get_json()
     username = data['username']
     password = data['password']
     print(data)
-    auth_response = requests.post(f"http://localhost:{authentication_service_port['port']}/auth/sign-in", json={'username': username, 'password': password})
+    
+    auth_response = requests.post(f"{authentication_service_url}/auth/sign-in", json={'username': username, 'password': password})
     print(auth_response.status_code)
+    
     if auth_response.status_code == 200:
         print('authenticate successfully')
         return jsonify({'authenticated': True, 'data': auth_response.json()})
@@ -23,9 +26,9 @@ def auth():
 @app.route('/status', methods=['GET'])
 def status():
     
-    # auth_response = request.get(authentication_service_port['port'])
+    auth_response = requests.get(f"{authentication_service_url}/status")
     
-    # if auth_response.status_code == 200:
+    if auth_response.status_code == 200:
         return jsonify({'status': 'OK'})
-    # else :
-        # return jsonify({'status': 'ERROR'}) 
+    else :
+        return jsonify({'status': 'ERROR'}) 
