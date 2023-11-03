da5702ce3568f5ce75968f1587e6e3a3b3752791
Golde34 <nguyendongducviet2001@gmail.com>
2023-11-03 18:08:33 +0700

    add gaia auth mechanism in frontend

--- GAIA/gaia_connector/app_router.py
+++ GAIA/gaia_connector/app_router.py
@@ -2,5 +2,3 @@ from controllers import app
 
 if __name__ == "__main__":
     app.run(host='localhost', port=3000, debug=True)
-
-from controllers import auth_service_controller, task_manager_controller, gaia_connector    
---
+++ GAIA/gaia_connector/configs/gaia_resources.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+gaia_resource_path = Path(__file__).parent.parent.parent / 'gaia_bot' / 'modules' / 'local' / 'resources'
