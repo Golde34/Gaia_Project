804f1de91883e7745157a8105b550d8718f0495d
Golde34 <nguyendongducviet2001@gmail.com>
2023-11-13 23:04:30 +0700

    create project create button

--- client_gui/src/components/subComponents/CardButton.jsx
+++ client_gui/src/components/subComponents/CardButton.jsx
@@ -8,11 +8,12 @@ const CardButton = (props) => {
     return (
         <Card className="w-xs" decoration="top" decorationColor="indigo">
             <Flex justifyContent="between" alignItems="center">
-                <Text>{props.name}</Text>
-                <BadgeDelta deltaType="moderateIncrease">+12.5%</BadgeDelta>    
+                <Metric>{props.name}</Metric>
+                <BadgeDelta deltaType="moderateIncrease">+12.5%</BadgeDelta>
             </Flex>
-            <Metric> {props.description} </Metric>
-            <Flex justifyContent="end">
+
+            <Flex className="mt-4">
+                <Text> {props.description} </Text>
                 <Button variant="primary" className="p-2" color="indigo"
                     type="button" onClick={() => navigate(props.url)}
                 > {props.buttonText}
--- client_gui/src/views/Project.jsx
+++ client_gui/src/views/Project.jsx
@@ -4,38 +4,46 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProjects } from "../store/actions/task_manager/project.actions";
 import Template from "./template";
 import CardButton from "../components/subComponents/CardButton";
-import { Metric } from "@tremor/react";
+import { Button, Card, Metric, Text, Title } from "@tremor/react";
 
 function ContentArea() {
     const dispatch = useDispatch();
     const listProjects = useSelector((state) => state.projectList);
     const { loading, error, projects } = listProjects;
-   
+
     useEffect(() => {
         dispatch(getProjects());
     }, [dispatch]);
- 
+
     return (
         <div>
-            { loading ? (
-                <p> Loading </p>    
+            {loading ? (
+                <p> Loading </p>
             ) : error ? (
                 <p> Error </p>
             ) : (
                 <>
-                <Metric style={{marginBottom:'30px', marginTop:'30px'}} 
-                    className="text-2xl font-bold text-gray-800"> Projects 
-                </Metric>
-                <div className="grid md:grid-cols-3 w-full"> 
-                    {projects.map((project) => (
-                        <CardButton name={project.name} description={project.description} 
-                            url={`/project/${project._id}`} buttonText="View Project"
-                        />
-                    ))}
-                </div>
+                    <Metric style={{ marginBottom: '30px', marginTop: '30px' }}
+                        className="text-2xl font-bold text-gray-800"> Projects
+                    </Metric>
+                    <div className="grid md:grid-cols-3 w-full h-full items-center">
+                        {projects.map((project) => (
+                            <div key={project._id} className="m-3">
+                                <CardButton name={project.name} description={project.description}
+                                    url={`/project/${project._id}`} buttonText="View Project"
+                                />
+                            </div>
+                        ))}
+                        <div key={'create-project'} className="m-3 flex justify-center">
+                            <Card className="flex flex-col justify-center items-center border-dashed border-2 border-sky-500 hover:border-solid hover:cursor-pointer text-center font-bold w-full h-full">
+                                <Title> Create Project </Title>
+                            </Card>
+                        </div>
+                    </div>
+
                 </>
             )
-        }
+            }
         </div>
     )
 }
