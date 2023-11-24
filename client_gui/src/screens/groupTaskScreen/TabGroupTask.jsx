d2c7c6f7dd38f819979fe0e4d299b08ef2000ea9
DongViet <82593925+Golde34@users.noreply.github.com>
2023-11-24 08:24:41 +0700

    test group task display

--- client_gui/src/screens/groupTaskScreen/TabGroupTask.jsx
+++ client_gui/src/screens/groupTaskScreen/TabGroupTask.jsx
@@ -1,10 +1,9 @@
 import { ArrowCircleRightIcon } from "@heroicons/react/solid";
-import { Card, Col, Flex, Grid, ProgressBar, Tab, TabGroup, TabList, TabPanel, TabPanels, Text } from "@tremor/react"
+import { Card, Tab, TabGroup, TabList, TabPanels } from "@tremor/react"
 import { useState } from "react";
 import EllipsisMenu from "../../components/EllipsisMenu";
 import { CreateNewGroupTask } from "./CreateNewGroupTask";
-import { CreateTaskDialog } from "../taskScreen/CreateTaskDialog";
-import TaskList from "../taskScreen/TaskList";
+import GroupTaskPanel from "./GroupTaskPanel";
 
 const TabGroupTask = (props) => {
     const groupTasks = props.groupTasks;
@@ -44,32 +43,9 @@ const TabGroupTask = (props) => {
                 </TabList>
                 <TabPanels>
                     {groupTasks.map((groupTask) => (
-                        <>
-                            <TabPanel key={groupTask._id}>
-                                <div className="mt-10">
-                                    <Grid numItems={12} className="gap-2">
-                                        <Col numColSpan={10}>
-                                            <Flex className="mt-4">
-                                                <Text className="w-full">{groupTask.description}</Text>
-                                                <Flex className="space-x-2" justifyContent="end">
-                                                    {/* TODO: NUMBER OF TOTAL TASKS AND TASKS DONE -> CALCULATE PERCENTAGE */}
-                                                    <Text>38% TASKS DONE / TOTAL TASKS</Text>
-                                                </Flex>
-                                            </Flex>
-                                            <ProgressBar value={38} className="mt-2 w-300" />
-                                        </Col>
-                                        <Col numColSpan={2} className="mt-4">
-                                            <div className="flex justify-center">
-                                                <CreateTaskDialog groupTaskId={groupTask._id}/>
-                                            </div>
-                                        </Col>
-                                    </Grid>
-                                </div>
-                            </TabPanel>
-                        </>
+                        <GroupTaskPanel groupTask={groupTask} />
                     ))}
                 </TabPanels>
-                <TaskList groupTaskId={groupTaskId} />
             </TabGroup>
         </Card>
     )
