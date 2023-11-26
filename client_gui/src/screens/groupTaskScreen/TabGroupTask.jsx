c4439b09501606d457b3e3d021f88faa6cdf58dc
Golde34 <nguyendongducviet2001@gmail.com>
2023-11-26 19:03:31 +0700

    fix frontend

--- client_gui/src/screens/groupTaskScreen/TabGroupTask.jsx
+++ client_gui/src/screens/groupTaskScreen/TabGroupTask.jsx
@@ -1,19 +1,20 @@
 import { ArrowCircleRightIcon } from "@heroicons/react/solid";
-import { Card, Tab, TabGroup, TabList, TabPanels } from "@tremor/react"
+import { Card, Col, Flex, Grid, ProgressBar, Tab, TabGroup, TabList, TabPanel, TabPanels, Text } from "@tremor/react"
 import { useState } from "react";
 import EllipsisMenu from "../../components/EllipsisMenu";
 import { CreateNewGroupTask } from "./CreateNewGroupTask";
-import GroupTaskPanel from "./GroupTaskPanel";
+import { CreateTaskDialog } from "../taskScreen/CreateTaskDialog";
+import TaskList from "../taskScreen/TaskList";
 
 const TabGroupTask = (props) => {
     const groupTasks = props.groupTasks;
-    
+
     const [activeTab, setActiveTab] = useState(groupTasks[0]._id);
-    const [groupTaskId, setGroupTaskId] = useState(groupTasks[0]._id);
+    const [selectedGroupTaskId, setSelectedGroupTaskId] = useState(groupTasks[0]._id);
 
     const handleTabChange = (tabId) => {
         setActiveTab(tabId);
-        setGroupTaskId(tabId);
+        setSelectedGroupTaskId(tabId);
     }
 
     return (
@@ -43,7 +44,30 @@ const TabGroupTask = (props) => {
                 </TabList>
                 <TabPanels>
                     {groupTasks.map((groupTask) => (
-                        <GroupTaskPanel groupTask={groupTask} />
+                        <TabPanel key={groupTask._id}>
+                            <div className="mt-10">
+                                <Grid numItems={12} className="gap-2">
+                                    <Col numColSpan={10}>
+                                        <Flex className="mt-4">
+                                            <Text className="w-full">{groupTask.description}</Text>
+                                            <Flex className="space-x-2" justifyContent="end">
+                                                {/* TODO: NUMBER OF TOTAL TASKS AND TASKS DONE -> CALCULATE PERCENTAGE */}
+                                                <Text>38% TASKS DONE / TOTAL TASKS</Text>
+                                            </Flex>
+                                        </Flex>
+                                        <ProgressBar value={38} className="mt-2 w-300" />
+                                    </Col>
+                                    <Col numColSpan={2} className="mt-4">
+                                        <div className="flex justify-center">
+                                            <CreateTaskDialog groupTaskId={groupTask._id} />
+                                        </div>
+                                    </Col>
+                                </Grid>
+                            </div>
+                            {selectedGroupTaskId && (
+                                <TaskList groupTaskId={selectedGroupTaskId} />
+                            )}
+                        </TabPanel>
                     ))}
                 </TabPanels>
             </TabGroup>
