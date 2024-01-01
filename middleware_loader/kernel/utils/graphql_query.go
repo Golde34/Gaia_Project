e356176c69f30ee3f47b6e65e4607ef0bb0aa651
Golde34 <nguyendongducviet2001@gmail.com>
2024-01-01 18:03:33 +0700

    dynamic generate graphql query

--- middleware_loader/kernel/utils/graphql_query.go
+++ middleware_loader/kernel/utils/graphql_query.go
@@ -1,14 +1,18 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"middleware_loader/infrastructure/graph/model"
+	"net/http"
 	"reflect"
 	"strings"
 )
 
-func GenerateGraphQLQuery(action string, function string, input interface{}, output interface{}) (string) {
+func GenerateGraphQLQueryWithInput(action string, function string, input interface{}, output interface{}) (string) {
 	// Convert input
 	inputMap := make(map[string]interface{})
 	inrec, _ := json.Marshal(input)
@@ -37,6 +41,51 @@ func GenerateGraphQLQuery(action string, function string, input interface{}, out
 	return query
 }
 
+func GenerateGraphQLQueryNoInput(action string, function string, output interface{}) (string) {
+	//Convert output
+	outputValue := reflect.ValueOf(output)
+	outputKeys := make([]string, outputValue.NumField())
+	for i:=0; i<outputValue.NumField(); i++ {
+		outputKeys[i] = outputValue.Type().Field(i).Tag.Get("json")
+	}
+	outputStr := strings.Join(outputKeys, ", ")
+
+	// Generate query
+	query := fmt.Sprintf(`%s { 
+		%s {
+			%s
+		}
+	}`, action, function, outputStr)
+	
+	return query
+}
+
+func ConnectToGraphQLServer(w http.ResponseWriter, query string) {
+	// Wrap the query in a JSON object
+	jsonQuery := map[string]string{
+		"query": query,
+	}
+
+	// Encode the JSON object into a buffer
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(jsonQuery); err != nil {
+		log.Printf("err: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//Send the query to the GraphQL server
+	resp, err := http.Post("http://localhost:4000/query", "application/json", &buf)
+	if err != nil {
+		log.Printf("err: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer resp.Body.Close()
+
+	io.Copy(w, resp.Body)
+}
+
 func main() {
 	action := "mutation"
 	function := "signin"
@@ -46,6 +95,6 @@ func main() {
 	}
 	output := model.AuthToken{}
 
-	query := GenerateGraphQLQuery(action, function, input, output)
+	query := GenerateGraphQLQueryWithInput(action, function, input, output)
 	fmt.Println(query)
 }
--- middleware_loader/ui/controller_services/auth_controller.go
+++ middleware_loader/ui/controller_services/auth_controller.go
@@ -1,10 +1,7 @@
 package controller_services
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
-	"log"
 	"middleware_loader/core/services"
 	"middleware_loader/infrastructure/graph/model"
 	"middleware_loader/kernel/utils"
@@ -18,30 +15,11 @@ func Signin(w http.ResponseWriter, r *http.Request, authService *services.AuthSe
 		return
 	}
 
-	// Convert the signin input into a GraphQL query
-	query := utils.GenerateGraphQLQuery("mutation", "signin", input, model.AuthToken{})	
+	query := utils.GenerateGraphQLQueryWithInput("mutation", "signin", input, model.AuthToken{})	
+	utils.ConnectToGraphQLServer(w, query)
+}
 
-	// Wrap the query in a JSON object
-	jsonQuery := map[string]string{
-		"query": query,
-	}
-
-	// Encode the JSON object into a buffer
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(jsonQuery); err != nil {
-		log.Printf("err: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	//Send the query to the GraphQL server
-	resp, err := http.Post("http://localhost:4000/query", "application/json", &buf)
-	if err != nil {
-		log.Printf("err: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	defer resp.Body.Close()
-
-	io.Copy(w, resp.Body)
+func Status(w http.ResponseWriter, r *http.Request, authService *services.AuthService) {
+	query := utils.GenerateGraphQLQueryNoInput("query", "status", model.User{})	
+	utils.ConnectToGraphQLServer(w, query)
 }
