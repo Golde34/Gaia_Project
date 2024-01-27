8115bdf847086ebc57f2cf7206eb5ea245484e74
Golde34 <nguyendongducviet2001@gmail.com>
2024-01-27 23:35:29 +0700

    bottom navbar mobile app

--- mobile_app/lib/components/bottom_nav_bar.dart
+++ mobile_app/lib/components/bottom_nav_bar.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({super.key});
+class GaiaBottomNavBar extends StatelessWidget {
+  void Function(int?) onTabChange;
+  GaiaBottomNavBar({super.key, required this.onTabChange,});
 
   @override
   Widget build(BuildContext context) {
-    return GNav(
-      color: Colors.grey[400],
-      tabs: [
-        GButton(
-          icon: Icons.home,
-          text: 'Auth',
-        ),
-        GButton(
-          icon: Icons.add_box,
-          text: 'Project',
-        ),
-      ]
+    return Container(
+      margin: const EdgeInsets.all(25),
+      child: GNav(
+          onTabChange: (value) => onTabChange(value),
+          color: Colors.grey[400],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          activeColor: const Color.fromARGB(255, 20, 33, 117),
+          tabBackgroundColor: Colors.indigo.shade300,
+          tabBorderRadius: 24,
+          tabActiveBorder: Border.all(color: Colors.black26),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          tabs: const [
+            GButton(
+              icon: Icons.home_sharp,
+              text: 'Home',
+              
+            ),
+            GButton(
+              icon: Icons.shield_sharp,
+              text: 'Auth',
+            ),
+            GButton(
+              icon: Icons.settings_sharp,
+              text: 'Setting',
+            ),
+          ]),
     );
   }
 }
--- mobile_app/lib/pages/home_page.dart
+++ mobile_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/components/bottom_nav_bar.dart';
 import 'package:mobile_app/configs/const.dart';
+import 'package:mobile_app/pages/auth_page.dart';
+import 'package:mobile_app/pages/setting_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,20 +12,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // navigate bottom bar
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages
+  final List<Widget> _pages =[
+    HomePage(),
+    AuthPage(),
+    SettingPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: GaiaBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index!),
+      ),
       // appBar: AppBar(
       //   title: Text('Home Page'),
       //   elevation: 0,
       // ),
-      body: ListView(
-        children: [
-          // ChuChu(),
-        ],
-      )
+      body: _pages[_selectedIndex],
     );
   }
 }
