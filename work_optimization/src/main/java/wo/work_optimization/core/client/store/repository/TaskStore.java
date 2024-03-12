f07a406f10b4b6fc7dbe8508dfcca7740c4a57c7
Golde34 <nguyendongducviet2001@gmail.com>
2024-03-13 01:23:39 +0700

    repo flow

---
+++ work_optimization/src/main/java/wo/work_optimization/core/client/store/repository/TaskStore.java
@@ -0,0 +1,7 @@
+package wo.work_optimization.core.client.store.repository;
+
+import wo.work_optimization.infrastructure.repository.TaskRepository;
+
+public interface TaskStore extends TaskRepository {
+    
+}
