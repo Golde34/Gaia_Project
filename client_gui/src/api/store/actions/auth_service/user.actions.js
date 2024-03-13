186ebeaa3272f32b5f02f9bfa25f7994e8a99019
Golde34 <nguyendongducviet2001@gmail.com>
2024-03-13 17:14:40 +0700

    init list user screen

--- client_gui/src/api/store/actions/auth_service/user.actions.js
+++ client_gui/src/api/store/actions/auth_service/user.actions.js
@@ -1,4 +1,5 @@
-import { USER_LIST_REQUEST } from "../../constants/auth_service/user.constants";
+import { HttpMethods, serverRequest } from "../../../baseAPI";
+import { USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS } from "../../constants/auth_service/user.constants";
 
 const portName = {
     authPort: 'authenticationServicePort',
@@ -8,7 +9,7 @@ export const getUsers = () => async (dispatch) => {
     dispatch({ type: USER_LIST_REQUEST });
     try {
         const { data } = await serverRequest('/user/getAllUsers', HttpMethods.GET, portName.authPort, null);
-        dispatch({ type: USER_LIST_SUCCESS, payload: data.data });
+        dispatch({ type: USER_LIST_SUCCESS, payload: data });
     } catch (error) {
         dispatch({
             type: USER_LIST_FAIL,
