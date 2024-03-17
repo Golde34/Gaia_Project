808fd9cb3b60eaca752af731290d24f80e697801
Golde34 <nguyendongducviet2001@gmail.com>
2024-03-17 16:27:25 +0700

    update user crud management

--- client_gui/src/screens/authManagerScreen/UserListScreen.jsx
+++ client_gui/src/screens/authManagerScreen/UserListScreen.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useCallback, useEffect, useRef } from "react";
 import { getUsers } from "../../api/store/actions/auth_service/user.actions";
-import { Card, Table, TableBody, TableHead, TableHeaderCell, TableRow, Title } from "@tremor/react";
+import { Button, Card, Flex, Table, TableBody, TableHead, TableHeaderCell, TableRow, Title } from "@tremor/react";
 
 const UserListScreen = () => {
     const dispatch = useDispatch();
@@ -29,31 +29,47 @@ const UserListScreen = () => {
             ) : error ? (
                 <p>{error}</p>
             ) : (
-                <Card>
-                    <Title>List of Users</Title>
-                    <Table className="mt-5">
-                        <TableHead>
-                            <TableRow>
-                                <TableHeaderCell><Title>Id</Title></TableHeaderCell>
-                                <TableHeaderCell><Title>Name</Title></TableHeaderCell>
-                                <TableHeaderCell><Title>Username</Title></TableHeaderCell>
-                                <TableHeaderCell><Title>Email</Title></TableHeaderCell>
-                                <TableHeaderCell><Title>Last Login</Title></TableHeaderCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {users.map((user) => (
-                                <TableRow key={user.id}>
-                                    <TableHeaderCell>{user.id}</TableHeaderCell>
-                                    <TableHeaderCell>{user.name}</TableHeaderCell>
-                                    <TableHeaderCell>{user.username}</TableHeaderCell>
-                                    <TableHeaderCell>{user.email}</TableHeaderCell>
-                                    <TableHeaderCell>{user.lastLogin}</TableHeaderCell>
+                <>
+                    <Card>
+                        <Flex>
+                            <Title>Action</Title>
+                            <Button>Add User</Button>
+                        </Flex>
+                        <Flex className="mt-5">
+                            <Title>Search</Title>
+                        </Flex>
+                    </Card>
+                    <Card>
+                        <Title>List of Users</Title>
+                        <Table className="mt-5">
+                            <TableHead>
+                                <TableRow>
+                                    <TableHeaderCell><Title>Id</Title></TableHeaderCell>
+                                    <TableHeaderCell><Title>Name</Title></TableHeaderCell>
+                                    <TableHeaderCell><Title>Username</Title></TableHeaderCell>
+                                    <TableHeaderCell><Title>Email</Title></TableHeaderCell>
+                                    <TableHeaderCell><Title>Last Login</Title></TableHeaderCell>
+                                    <TableHeaderCell><Title>Edit User</Title></TableHeaderCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Card>
+                            </TableHead>
+                            <TableBody>
+                                {users.map((user) => (
+                                    <TableRow key={user.id}>
+                                        <TableHeaderCell>{user.id}</TableHeaderCell>
+                                        <TableHeaderCell>{user.name}</TableHeaderCell>
+                                        <TableHeaderCell>{user.username}</TableHeaderCell>
+                                        <TableHeaderCell>{user.email}</TableHeaderCell>
+                                        <TableHeaderCell>{user.lastLogin}</TableHeaderCell>
+                                        <TableHeaderCell>
+                                            <Button className="me-2">Edit</Button>
+                                            <Button>Delete</Button>
+                                        </TableHeaderCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Card>
+                </>
             )
             }
         </>
