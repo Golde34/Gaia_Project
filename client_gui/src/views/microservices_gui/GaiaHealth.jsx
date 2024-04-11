17e02c0619b66263d71908566d48115b1f60ae53
DongViet <82593925+Golde34@users.noreply.github.com>
2024-04-11 21:36:06 +0700

    Update GaiaHealth.jsx

--- client_gui/src/views/microservices_gui/GaiaHealth.jsx
+++ client_gui/src/views/microservices_gui/GaiaHealth.jsx
@@ -2,7 +2,6 @@ export const GaiaHealth = () => {
     return (
         <>
             <h1>GaiaHealth</h1>
-            <p>This function must check the health of the server, number of instances, gpu, cpu, etc... It must be a long way to Gaia can reach to this function =)))))</p>
         </>
     )
 }
