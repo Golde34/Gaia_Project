0f42106b3329619a4215570a4fd2ea0001b3179f
Golde34 <nguyendongducviet2001@gmail.com>
2024-05-04 22:06:34 +0700

    change service port

--- authentication_manager/auth_service/src/main/java/auth/authentication_service/core/services/PrivilegeServiceImpl.java
+++ authentication_manager/auth_service/src/main/java/auth/authentication_service/core/services/PrivilegeServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
