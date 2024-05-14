97e08d28d87392c6cb4d52c192c8cea27ea29113
Golde34 <nguyendongducviet2001@gmail.com>
2024-05-14 21:46:58 +0700

    fix rest template

--- work_optimization/src/main/java/wo/work_optimization/infrastructure/config/RestConfig.java
+++ work_optimization/src/main/java/wo/work_optimization/infrastructure/config/RestConfig.java
@@ -1,17 +1,5 @@
 package wo.work_optimization.infrastructure.config;
 
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,20 +9,12 @@ import org.springframework.web.client.RestTemplate;
 
 import wo.work_optimization.infrastructure.client.feign.RestExceptionHandler;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RestConfig {
     @Value("${rest.default.connection.connect-timeout}")
     private int connectionTimeout;
-
-    private static final boolean IS_HOST_NAME_VERIFIER = true;
 
     @Bean(name = "woRestTemplate")
     public RestTemplate createDefaultRestTemplate() {
@@ -43,27 +23,5 @@ public class RestConfig {
         restTemplate.getMessageConverters().add(1, new FormHttpMessageConverter());
         restTemplate.setErrorHandler(new RestExceptionHandler());
         return restTemplate;
-    }
-
-    private HttpClientBuilder poolingHttpClientConfigBuilder() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(getSocketFactoryRegistry());
-        connectionManager.setMaxTotal(2048);
-        connectionManager.setDefaultMaxPerRoute(1024);
-
-        RequestConfig config = RequestConfig.custom().setConnectTimeout(connectionTimeout).setConnectionRequestTimeout(connectionTimeout).setSocketTimeout(connectionTimeout).build();
-
-        return HttpClients.custom().setConnectionManager(connectionManager).evictExpiredConnections().evictIdleConnections(30L, TimeUnit.SECONDS).setDefaultRequestConfig(config).setRetryHandler(new DefaultHttpRequestRetryHandler(0, false)).disableRedirectHandling();
-    }
-
-    private static Registry<ConnectionSocketFactory> getSocketFactoryRegistry() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        SSLContextBuilder builder = SSLContexts.custom();
-        builder.loadTrustMaterial(null, (chain, authType) -> true);
-        SSLContext sslContext = builder.build();
-        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, getHostnameVerifier());
-        return RegistryBuilder.<ConnectionSocketFactory>create().register("https", sslSocketFactory).register("http", new PlainConnectionSocketFactory()).build();
-    }
-
-    private static HostnameVerifier getHostnameVerifier() {
-        return (s, sslSession) -> IS_HOST_NAME_VERIFIER;
     }
 }
