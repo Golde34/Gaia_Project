c4f02150ce67fea5b861f4547dcb0cdd53a40082
Golde34 <nguyendongducviet2001@gmail.com>
2024-05-17 23:48:21 +0700

    add schedule plan main function

--- schedule_plan/src/index.ts
+++ schedule_plan/src/index.ts
@@ -1,1 +1,36 @@
-console.log("hello world!")
+import express, { Application, NextFunction, Request, Response } from "express";
+import { config, validateEnvironmentVars } from "./infrastructure/config";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+async function main(): Promise<void> {
+    validateEnvironmentVars();
+
+    const app: Application = express();
+
+    app.use(
+        bodyParser.urlencoded({
+            parameterLimit: 100000,
+            limit: '50mb',
+            extended: true,
+        }),
+    );
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan('dev'));
+
+    app.get("/status", (req: Request, res: Response) => {
+        res.status(200).send("OK");
+    })
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        next(new Error("Not Found"));
+    });
+
+    app.listen(config.server.listenPort, () => {
+        console.log(`Server is running on port ${config.server.listenPort}`);
+    })
+}
---
+++ schedule_plan/src/infrastructure/config.ts
@@ -0,0 +1,74 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: './src/.env'});
+
+const REQUIRED_ENV_VARS = [
+    'LISTEN_PORT',
+    'DATABASE_HOST',
+    'DATABASE_NAME',
+    'DATABASE_PORT',
+    'DATABASE_USERNAME',
+    'DATABASE_PASSWORD',
+];
+
+interface Configuration {
+    database: {
+        host: string;
+        port: number;
+        name: string;
+        username: string;
+        password: string;
+    };
+    server: {
+        listenPort: number;
+    };
+    authServer: {
+        host: string;
+        port: number;
+    };
+    jwt: {
+        accessSecret: string;
+        refreshSecret: string;
+    };
+    kafka: {
+        bootstrapServers: string;
+        groupId: string;
+    }
+}
+
+export const config: Configuration = {
+    database: {
+        host: process.env.DATABASE_HOST ?? 'localhost',
+        port: Number(String(process.env.DATABASE_PORT)) ?? 27017,
+        name: process.env.DATABASE_NAME ?? 'task_database',
+        username: process.env.DATABASE_USERNAME ?? 'root',
+        password: process.env.DATABASE_PASSWORD ?? 'root',
+    },
+    server: {
+        listenPort: Number(String(process.env.LISTEN_PORT)) ?? 3000,
+    },
+    authServer: {
+        host: process.env.AUTH_SERVICE_HOST ?? 'localhost',
+        port: Number(String(process.env.AUTH_SERVICE_PORT)) ?? 4001,
+    },
+    jwt: {
+        accessSecret: String(process.env.ACCESS_SECRET),
+        refreshSecret: String(process.env.REFRESH_SECRET)
+    },
+    kafka: {
+        bootstrapServers: process.env.KAFKA_BOOTSTRAP_SERVERS ?? 'localhost:9094',
+        groupId: process.env.KAFKA_GROUP_ID ?? 'task-manager'
+    }
+};
+
+export const validateEnvironmentVars = (): void => {
+    const missingRequirements: string[] = [];
+    REQUIRED_ENV_VARS.forEach((envVar) => {
+        if (!(envVar in process.env)) {
+            missingRequirements.push(envVar);
+        }
+    });
+    if (missingRequirements.length !== 0) {
+        throw new Error(`Missing required environment variables: ${missingRequirements.join(', ')}`);
+    }
+}
