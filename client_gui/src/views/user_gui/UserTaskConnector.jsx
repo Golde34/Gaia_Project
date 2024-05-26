657cc5793a8a462e8f3ad38b96fa82a89cf7682b
Golde34 <nguyendongducviet2001@gmail.com>
2024-05-26 14:12:57 +0700

    add category bar in task registration screen

--- client_gui/src/views/user_gui/UserTaskConnector.jsx
+++ client_gui/src/views/user_gui/UserTaskConnector.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { Navigate } from "react-router-dom";
 import Template from "../../components/template/Template";
-import { Card, Col, Grid, Metric, NumberInput, Subtitle, Text, TextInput, Title } from "@tremor/react";
+import { Button, Card, CategoryBar, Col, Grid, Legend, Metric, NumberInput, Subtitle, Text, TextInput, Title } from "@tremor/react";
 
 function ContentArea(props) {
     const redirectPage = props.redirectPage;
@@ -40,7 +40,9 @@ function ContentArea(props) {
     function handleWorkTime(newSleepTime, newRelaxTime, newEatTime, newTravelTime) {
         setWorkTime(24 - newSleepTime - newRelaxTime - newEatTime - newTravelTime);
     }
-
+    function convertTimeToPercents(time) {
+        return time * 100 / 24;
+    }
     return (
         <>
             {loading ? (
@@ -56,35 +58,48 @@ function ContentArea(props) {
                     <Metric style={{ marginBottom: '30px', marginTop: '30px' }}
                         className="text-2xl font-bold text-gray-800">Task Registration
                     </Metric>
-                    <Grid numItems={4} className="gap-2">
-                        <Col numColSpan={4}>
+                    <Grid numItems={12} className="gap-2">
+                        <Col numColSpan={12}>
                             <Card className="mt-5" style={{ textAlign: "start" }}>
                                 <Title className="text-xl">Introduce</Title>
                                 <Text>First you need to calculate your work time by your self. Base on your estimate we will optimize your tasks durings your work day.</Text>
                             </Card>
                         </Col>
-                        <Col numColSpan={2}>
+                        <Col numColSpan={6}>
                             <Card className="mt-4">
-                                <Title>How much time do you spend sleeping?</Title>
-                                <NumberInput
-                                    className="mt-3"
-                                    placeholder="Enter your sleep time..."
-                                    min={0}
-                                    max={24}
-                                    value={sleepTime}
-                                    onChange={e => {
-                                        const newSleepTime = e.target.value;
-                                        setSleepTime(newSleepTime)
-                                        handleWorkTime(newSleepTime, relaxTime, eatTime, travelTime)
-                                    }}
-                                    error={formatNumber(sleepTime)}
-                                    errorMessage="Please enter a valid number between 0 and 24"
-                                />
-                                <Title className="mt-4">
-                                    What time do you usually sleep?
-                                </Title>
-                                <Grid numItems={2}>
-                                    <Col numColSpan={1}>
+                                <Grid numItems={6}>
+                                    <Col numColSpan={6}>
+                                        <Title>How much time do you spend sleeping?</Title>
+                                    </Col>
+                                    <Col numColSpan={2}>
+                                        <Legend
+                                            className="mt-4"
+                                            categories={['Work Time']}
+                                            colors={['emerald']}
+                                        />
+                                    </Col>
+                                    <Col numColSpan={4}>
+                                        <NumberInput
+                                            className="mt-3"
+                                            placeholder="Enter your sleep time..."
+                                            min={0}
+                                            max={24}
+                                            value={sleepTime}
+                                            onChange={e => {
+                                                const newSleepTime = e.target.value;
+                                                setSleepTime(newSleepTime)
+                                                handleWorkTime(newSleepTime, relaxTime, eatTime, travelTime)
+                                            }}
+                                            error={formatNumber(sleepTime)}
+                                            errorMessage="Please enter a valid number between 0 and 24"
+                                        />
+                                    </Col>
+                                    <Col numColSpan={6}>
+                                        <Title className="mt-4">
+                                            What time do you usually sleep?
+                                        </Title>
+                                    </Col>
+                                    <Col numColSpan={3}>
                                         <TextInput
                                             className="me-2 mt-3"
                                             style={{ backgroundColor: "white", color: "black" }}
@@ -92,7 +107,7 @@ function ContentArea(props) {
                                             type="time"
                                         />
                                     </Col>
-                                    <Col numColSpan={1}>
+                                    <Col numColSpan={3}>
                                         <TextInput
                                             className="ms-2 mt-3 text-white"
                                             style={{ backgroundColor: "white", color: "black" }}
@@ -103,15 +118,21 @@ function ContentArea(props) {
                                 </Grid>
                             </Card>
                         </Col>
-                        <Col numColSpan={2}>
+                        <Col numColSpan={6}>
                             <Card className="mt-4">
                                 <Title>How much time do you spend relaxing, eating, and traveling?</Title>
                                 <Grid numItems={6}>
-                                    <Col numColSpan={1}><Subtitle className="mt-5">Relaxing</Subtitle></Col>
-                                    <Col numColSpan={5}>
+                                    <Col numColSpan={2}>
+                                        <Legend
+                                            className="mt-4"
+                                            categories={['Relaxing']}
+                                            colors={['blue']}
+                                        />
+                                    </Col>
+                                    <Col numColSpan={4}>
                                         <NumberInput
                                             className="mt-4"
-                                            placeholder="Enter your sleep time..."
+                                            placeholder="Enter your relax time..."
                                             min={0}
                                             max={24}
                                             value={relaxTime}
@@ -124,8 +145,14 @@ function ContentArea(props) {
                                             errorMessage="Please enter a valid number between 0 and 24"
                                         />
                                     </Col>
-                                    <Col numColSpan={1}><Subtitle className="mt-3">Eating</Subtitle></Col>
-                                    <Col numColSpan={5}>
+                                    <Col numColSpan={2}>
+                                        <Legend
+                                            className="mt-2"
+                                            categories={['Eating']}
+                                            colors={['yellow']}
+                                        />
+                                    </Col>
+                                    <Col numColSpan={4}>
                                         <NumberInput
                                             className="mt-2"
                                             placeholder="Enter your eat time..."
@@ -141,11 +168,17 @@ function ContentArea(props) {
                                             errorMessage="Please enter a valid number between 0 and 24"
                                         />
                                     </Col>
-                                    <Col numColSpan={1}><Subtitle className="mt-3">Traveling</Subtitle></Col>
-                                    <Col numColSpan={5}>
+                                    <Col numColSpan={2}>
+                                        <Legend
+                                            className="mt-2"
+                                            categories={['Traveling']}
+                                            colors={['rose']}
+                                        />
+                                    </Col>
+                                    <Col numColSpan={4}>
                                         <NumberInput
                                             className="mt-2"
-                                            placeholder="Enter your sleep time..."
+                                            placeholder="Enter your travelling time..."
                                             min={0}
                                             max={24}
                                             value={travelTime}
@@ -161,11 +194,26 @@ function ContentArea(props) {
                                 </Grid>
                             </Card>
                         </Col>
-                        <Col numColSpan={4}>
+                        <Col numColSpan={12}>
                             <Card className="mt-4">
-                                <Title>Is this amount of time of your work? </Title>
-                                <Text>Your work time = 24 - sleepTime - relaxTime - travelTime</Text>
-                                <Text>{workTime}</Text>
+                                <Grid numItems={8}>
+                                    <Col numColSpan={3}>
+                                        <Title>Is this amount of time of your work? </Title>
+                                        <Text>Your work time = 24 - sleepTime - relaxTime - travelTime</Text>
+                                    </Col>
+                                    <Col numColSpan={5}>
+                                        <Title> {workTime} hours</Title>
+                                        <CategoryBar
+                                            values={[convertTimeToPercents(workTime), convertTimeToPercents(relaxTime),
+                                            convertTimeToPercents(eatTime), convertTimeToPercents(travelTime), convertTimeToPercents(sleepTime)]}
+                                            colors={['emerald', 'blue', 'yellow', 'rose', 'indigo']}
+                                            className="mt-4"
+                                            showLabels={false}
+                                            showAnimation={true}
+                                        />
+                                    </Col>
+                                </Grid>
+
                             </Card>
                         </Col>
                     </Grid>
