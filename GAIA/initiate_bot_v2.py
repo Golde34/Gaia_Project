ad8967756ee4beca76ce09b7435d38ddc4788fef
Golde34 <nguyendongducviet2001@gmail.com>
2024-06-09 00:17:19 +0700

    init bot flow

--- GAIA/initiate_bot_v2.py
+++ GAIA/initiate_bot_v2.py
@@ -1,2 +1,29 @@
-def process_bot_v2():
-    print("initiate_bot_v2 is called")
+import colorama
+from gaia_bot_v2.commands.authentication import AuthenticationCommand
+
+
+async def process_bot_v2(): 
+    # Initiate bot console
+    colorama.init()
+    print(f"Gaia version: 2.0.0")
+    # Startup
+    console_manager, assistant = _startup()
+     # Authen user
+    token = await AuthenticationCommand().process()
+    if token == None:
+        print("Authentication failed, process user to guess mode.")    
+        _process_guess_mode(console_manager, assistant)
+    print(token)
+    # initiate
+    await _initiate_gaia(console_manager=console_manager, assistant=assistant)
+
+
+def _startup():
+    pass
+
+def _process_guess_mode(console_manager, assistant):
+    pass
+
+async def _initiate_gaia(console_manager, assistant):
+    await print(console_manager, assistant)
+    pass
