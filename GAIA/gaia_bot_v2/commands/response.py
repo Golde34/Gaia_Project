bcd2671a90e1375055f56254580321a382a4fc01
Golde34 <nguyendongducviet2001@gmail.com>
2024-06-11 21:17:18 +0700

    fix response

--- GAIA/gaia_bot_v2/commands/response.py
+++ GAIA/gaia_bot_v2/commands/response.py
@@ -11,25 +11,18 @@ class AlpacaResponse():
     @classmethod
     def generate_response(cls, text, model, tokenize, **kwargs):
         try:
-            check_gpu = gpuutils.check_gpu_memory()
-            if check_gpu:
-                print("Generating response by alpaca")
-                response = inference.call_alpaca_response(inp=text, model=model, tokenizer=tokenize)
-                last_response = cls._format_response(response)
-                return last_response
-            else:
-                gpuutils.clear_gpu_memory()
-                print("GPU Mempry is not enough, holding the next model.")
-                return "Gaia is not available now. Open web browser to access Gaia's Service."
+            print("Generating response by alpaca")
+            response = inference.call_alpaca_response(inp=text, model=model, tokenizer=tokenize)
+            last_response = cls._format_response(response)
+            return last_response
         except Exception as e:
             print(f"Error: {e}")
-            cls.console_manager.console_output('Failed to generate response.')
+            return "Failed to generate response."
             
     @classmethod
     def _format_response(cls, response):
-        matches = re.findall(r'{(.+?)}', response)
-        if matches:
-            return matches[-1].strip()
-        
+        parts = response.split("### Response:\n")
+        if len(parts) > 1:
+            return parts[1].strip()
         return response
     
