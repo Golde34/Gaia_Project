4f1f83df74b6db268c192105f9b43d1b771fa3fb
Golde34 <nguyendongducviet2001@gmail.com>
2024-06-20 00:00:12 +0700

    test gaia auth

--- GAIA/gaia_bot_v2/commands/authentication.py
+++ GAIA/gaia_bot_v2/commands/authentication.py
@@ -1,5 +1,3 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 from gaia_bot_v2.kernel.configs.auth_config import USER_PROFILE
 
 
@@ -8,29 +6,6 @@ class AuthenticationCommand():
     def __init__(self, auth_service_status):
         self.token = None
         self.auth_service_status = auth_service_status
-   
-    async def authenticate(self):
-        loop = asyncio.new_event_loop()
-        with ThreadPoolExecutor() as executor:
-            # Tạo các future để chạy các phương pháp xác thực song song
-            futures = [
-                loop.run_in_executor(executor, self.face_recognition_method),
-                loop.run_in_executor(executor, self.voice_recognition_method),
-                loop.run_in_executor(executor, self.username_password_method),
-            ]
-            
-            # Đợi phương pháp đầu tiên hoàn thành
-            done, pending = await asyncio.wait(futures, return_when=asyncio.FIRST_COMPLETED)
-            # Hủy bỏ các tác vụ còn lại nếu một tác vụ đã hoàn thành thành công
-            for future in pending:
-                future.cancel()
-            
-            if result:
-                print("Authentication successful")
-            else:
-                print("Authentication failed")
-            # Lấy kết quả của phương pháp đầu tiên hoàn thành
-            result = done.pop().result()
     
     def process(self):
         try:
