4c9620e3f06028eaf4ae9cf6527871e103eaaaa2
Golde34 <nguyendongducviet2001@gmail.com>
2024-06-23 23:56:52 +0700

    fix gaia bot

--- GAIA/gaia_bot/modules/ports/bash_shells/gaia_connector.sh
+++ GAIA/gaia_bot/modules/ports/bash_shells/gaia_connector.sh
@@ -3,7 +3,9 @@
 # create a lock file
 touch /tmp/gaia_connector_lock
 
-python gaia_connector/app_router.py
+cd ../gaia_connector
+
+python app_router.py
 
 # remove the lock file when done
 rm -f /tmp/gaia_connector_lock
