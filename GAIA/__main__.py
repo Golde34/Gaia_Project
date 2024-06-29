4f382442d45bf9dc9a84febd456953f6a0939bf6
Golde34 <nguyendongducviet2001@gmail.com>
2024-06-29 13:27:31 +0700

    add arguments

--- GAIA/__main__.py
+++ GAIA/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 
+from gaia_bot.kernel.configs.__version__ import __version__
 
 async def main():
     parser = argparse.ArgumentParser(description="Gaia Bot's command line interface")
@@ -9,7 +10,7 @@ async def main():
                         choices=["response", "task_detection", "object_detection"], 
                         help="Train model")
     parser.add_argument("--gaia-help", "-gh", action="store_true", help="Gaia show help message")
-    # parser.add_argument('-v', '--version', action='version', version=f'Gaia version: ${__version__}')
+    parser.add_argument("--version", "-v", action="store_true", help=f"Show Gaia version")
     # parser.add_argument('-t', '--test', action='store_true', help='test mode')
     # parser.add_argument('-d', '--debug', action='store_true', help='debug mode')
     # parser.add_argument('-s', '--skill', action='store_true', help='skill mode')
@@ -31,6 +32,8 @@ async def main():
         model_switch_case(args.train_model) 
     if args.gaia_help or not any(vars(args).values()):
         parser.print_help()
+    if args.version:
+        print(f"Gaia version: {__version__}")
 
 if __name__ == "__main__":
     asyncio.run(main())
