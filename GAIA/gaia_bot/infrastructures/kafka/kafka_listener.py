01de7706d44d14a23c2330bd5464c2ca257275d5
Golde34 <nguyendongducviet2001@gmail.com>
2024-07-03 09:23:06 +0700

    add kafka listener decorator

--- GAIA/gaia_bot/infrastructures/kafka/kafka_listener.py
+++ GAIA/gaia_bot/infrastructures/kafka/kafka_listener.py
@@ -19,13 +19,15 @@ class KafkaConsumerListener:
 @KafkaConsumerListener(KafkaServiceName.CAMERA_CV.value)
 def handle_open_camera_space(service_name: str):
     consumer = registry_consumer(service_name)
+    handle_consumer_message(consumer=consumer, consumer_function=handle_camera_cv_message)
+    
 
-    # if msg.topic == 'OPEN_CAMERA_SPACE':
-    #     print(f"Handling OPEN_CAMERA_SPACE message: {msg.value}")
-    #     kafka_topics = load_kakfka_topic(KafkaServiceName.CAMERA_CV.value)
-    #     asyncio.run(consume(kafka_topics=kafka_topics, consumer_function=handle_camera_cv_message))
-    # if msg.topic == 'SHUTDOWN_CAMERA_SPACE':
-    #     print(f"Handling SHUTDOWN_CAMERA_SPACE message: {msg.value}")
-
-def handle_camera_cv_message(message, topic):
-    print(f"Handling camera_cv message with topic: {topic} and message: {message}")
+def handle_camera_cv_message(msg):
+    if msg.topic == 'OPEN_CAMERA_SPACE':
+        print(f"Handling OPEN_CAMERA_SPACE message: {msg.value}")
+    if msg.topic == 'SHUTDOWN_CAMERA_SPACE':
+        print(f"Handling SHUTDOWN_CAMERA_SPACE message: {msg.value}")
+        
+        
+if __name__ == "__main__":
+    asyncio.run(handle_open_camera_space())
