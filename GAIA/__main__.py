1aa5be796d111dcf95e15e3c53b81dd68f79dd4a
Golde34 <nguyendongducviet2001@gmail.com>
2024-07-03 09:27:21 +0700

    add kafka process

--- GAIA/__main__.py
+++ GAIA/__main__.py
@@ -15,6 +15,7 @@ async def main():
     parser.add_argument("-tm", "--train-model", type=str, 
                         choices=["response", "skill_detection", "object_detection"], 
                         help="Train model")
+    parser.add_argument("--kafka", "-k", action="store_true", help="Run kafka only")
     parser.add_argument("--version", "-v", action="store_true", help=f"Show Gaia version")
     # parser.add_argument("--version", "-v", type=int, default=1, help=f"Gaia version: {__version__}"
     # parser.add_argument('-t', '--test', action='store_true', help='test mode')
@@ -49,6 +50,10 @@ async def _process_arguments(args):
     if args.mode:
         print(f"You can only use the argument --mode in the process mode.")
         help_message()
+    if args.kafka:
+        from gaia_bot.infrastructures.kafka import kafka_listener
+        print("Kafka listener activate")
+        kafka_listener.handle_open_camera_space()
 
 if __name__ == "__main__":
     asyncio.run(main())
