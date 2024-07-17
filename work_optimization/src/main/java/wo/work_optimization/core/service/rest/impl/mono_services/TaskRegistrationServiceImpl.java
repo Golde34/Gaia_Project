3e64ee53bf52b68b2a1d3a5978d607044190bc91
Golde34 <nguyendongducviet2001@gmail.com>
2024-07-17 21:11:18 +0700

    validate task registration request

--- work_optimization/src/main/java/wo/work_optimization/core/service/rest/impl/mono_services/TaskRegistrationServiceImpl.java
+++ work_optimization/src/main/java/wo/work_optimization/core/service/rest/impl/mono_services/TaskRegistrationServiceImpl.java
@@ -2,7 +2,9 @@ package wo.work_optimization.core.service.rest.impl.mono_services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+import wo.work_optimization.core.domain.constant.Constants;
 import wo.work_optimization.core.domain.dto.request.TaskRegistrationRequestDTO;
 import wo.work_optimization.core.domain.dto.response.TaskResponseDTO;
 import wo.work_optimization.core.domain.dto.response.UserResponseDTO;
@@ -33,59 +35,52 @@ public class TaskRegistrationServiceImpl implements TaskRegistrationService {
 
     public GeneralResponse<?> registerWorkOptimization(TaskRegistrationRequestDTO request) {
         log.info("Validate request: {}", request);
-        // validate request
-        boolean requestValidation = validateRequest(request);
-        if (!requestValidation) {
-            return genericResponse.matchingResponseMessage(new GenericResponse<>("There is an error when registry task scheduling.", ResponseMessage.msg400));
+        Pair<String, Boolean> requestValidation = validateRequest(request);
+        if (!requestValidation.getSecond()) {
+            return genericResponse.matchingResponseMessage(new GenericResponse<>(requestValidation.getFirst(), ResponseMessage.msg400));
         }
-        // mapper request to entity
-        TaskRegistration taskRegistration = createRequest(request);
-        // store db
-        taskRegistrationStore.userRegisterTaskOperation(taskRegistration);
-        // return response
-        return genericResponse.matchingResponseMessage(new GenericResponse<>(taskRegistration, ResponseMessage.msg200));
 
+        TaskRegistration taskRegistration = createRequest(request);
+        taskRegistrationStore.userRegisterTaskOperation(taskRegistration);
+        return genericResponse.matchingResponseMessage(new GenericResponse<>(taskRegistration, ResponseMessage.msg200));
     }
     
-    private boolean validateRequest(TaskRegistrationRequestDTO request) {
-        if (dataUtils.isNullOrEmpty(request)) {
-            return false;
+    private Pair<String, Boolean> validateRequest(TaskRegistrationRequestDTO request) {
+        if (dataUtils.isNullOrEmpty(request)
+                || dataUtils.isNullOrEmpty(request.getUserId())
+                || dataUtils.isNullOrEmpty(request.getWorkTime())
+                || request.getWorkTime() <= 0
+        ) {
+            return Pair.of(Constants.ErrorMessage.INVALID_REQUEST, false);
         }
-        if (dataUtils.isNullOrEmpty(request.getUserId())) {
-            return false;
-        }
-        if (dataUtils.isNullOrEmpty(request.getWorkTime())) {
-            return false;
-        }
-        if (request.getWorkTime() <= 0) {
-            return false;
-        }
-        if (!checkExistedUser(request.getUserId())) {
-            return false;
+        if (!checkExistedUser(request.getUserId()).getSecond()) {
+            return Pair.of(Constants.ErrorMessage.USER_NOT_FOUND, false);
         }
         if (!validateCalculatedTimeInDay(request)) {
-            return false;
+            return Pair.of(Constants.ErrorMessage.TOTAL_TIME_IN_DAY_ERROR, false);
         }
-        return true;
+        return Pair.of(Constants.ErrorMessage.SUCCESS, true);
     }
-    private boolean checkExistedUser(Long userId) {
+    private Pair<String, Boolean> checkExistedUser(Long userId) {
         // check existed user in auth service
         UserResponseDTO response = authServiceClient.getExistedUser(userId);
         log.info("Check existed user in auth service: [{}] ", response.toString());
         if (dataUtils.isNullOrEmpty(response) || dataUtils.isNullOrEmpty(response.getId())) {
-            log.error("There is no information of user: {} in auth service", userId);
-            return false;
+            String error = "There is no information of user: " + userId + " in auth service";
+            log.error(error);
+            return Pair.of(error, false);
         }
 
         log.info("Check existed user in task registration: [{}] ", userId);
         Optional<TaskRegistration> taskRegistration = taskRegistrationStore.getTaskRegistrationByUserId(userId);
         if (taskRegistration.isPresent()) {
-            log.error("User has already registered task scheduling: {}", userId);
-            return false;
+            String error = "User has already registered task scheduling: " + userId;
+            log.error(error);
+            return Pair.of(error, false);
         }
 
         log.info("There is no information of user: {} in task registration. Can initiate task registration information", userId);
-        return true;
+        return Pair.of(Constants.ErrorMessage.OK, true);
     }
 
     private boolean validateCalculatedTimeInDay(TaskRegistrationRequestDTO request) {
