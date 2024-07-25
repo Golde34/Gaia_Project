336dc58f3121aa5c8307a1162caeef02a8c2d89b
Golde34 <nguyendongducviet2001@gmail.com>
2024-07-25 22:50:45 +0700

    refactor gaia lib

--- kafka_lib/java/src/main/java/kafka/lib/java/adapter/consumer/messagehandlers/KafkaMessageHandler.java
+++
@@ -1,6 +0,0 @@
-package kafka.lib.java.adapter.consumer.messagehandlers;
-
-public abstract class KafkaMessageHandler {
-     public abstract String getTopic();
-     public abstract void processMessage(String message, String topic);
-}
--- kafka_lib/java/src/main/java/kafka/lib/java/constants/Constant.java
+++
@@ -1,5 +0,0 @@
-package kafka.lib.java.constants;
-public class Constant {
-    public static final String CORRELATION_ID_LOG_VAR_NAME = "correlationID";
-    public static final String DEFAULT_KAFKA_TEMPLATE = "defaultKafkaTemplate";
-}
