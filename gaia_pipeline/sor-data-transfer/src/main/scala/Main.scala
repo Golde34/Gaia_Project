87aac1554b6b7c5b2b5503a834a8bcaf1a91f1ce
Golde34 <nguyendongducviet2001@gmail.com>
2024-08-18 17:53:39 +0700

    refactor code

--- gaia_pipeline/sor-data-transfer/src/main/scala/Main.scala
+++ gaia_pipeline/sor-data-transfer/src/main/scala/Main.scala
@@ -2,13 +2,9 @@ import ujson._
 import scala.collection.mutable.ArrayBuffer
 import os._
 import services.SORDataTransfer
-import com.typesafe.config.ConfigFactory
 
 object DataPipeline {
-
   def main(args: Array[String]): Unit = {
-    val config = ConfigFactory.load()
-    println(config.getConfig("slick.dbs.default").root().render()) // Debug output để kiểm tra cấu hình
     SORDataTransfer.writeOutputToJSONFile()  
   }
 }
