82dc10c408c0c08ba8524a968d566e7ba7ad1526
Golde34 <nguyendongducviet2001@gmail.com>
2024-08-24 19:33:11 +0700

    load config from .env file

--- gaia_connector/ui/__init__.py
+++ gaia_connector/ui/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 from flask_kafka import FlaskKafka
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["KAFKA_CONFIG"] = {'bootstrap.servers': 'localhost:9094',
-                               'group.id': 'gaia-connector',
-                               'enable.auto.commit': 'false',
-                               'auto.offset.reset': 'earliest'}
+app.config["KAFKA_CONFIG"] = {'bootstrap.servers': os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9094"),
+                               'group.id': os.getenv("KAFKA_GROUP_ID", "gaia-connector"),
+                               'enable.auto.commit': os.getenv("KAFKA_ENABLE_AUTO_COMMIT", "false"),
+                               'auto.offset.reset': os.getenv("KAFKA_AUTO_OFFSET_RESET", "earliest"),}
 
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
