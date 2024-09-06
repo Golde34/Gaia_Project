ef93d279678ae2ff8a535cc7da65a60ea407aef5
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-06 19:42:19 +0700

    fix skill process

--- GAIA/__access_abilities__.py
+++ GAIA/__access_abilities__.py
@@ -5,6 +5,7 @@ from gaia_bot.kernel.utils.trie_node import build_trie_from_skills
 assistant_skill = AssistantSkill()
 
 def access_abilities_privately(skills):
-    tag_skill = str(input("Enter the skill you want to access: "))
+    text = str(input("Enter the skill you want to access: "))
+    tag_skill = "Create task"
     trie_skills = build_trie_from_skills(skills)
-    assistant_skill.skill_process(tag_skill, trie_skills)
+    assistant_skill.skill_process(tag_skill, trie_skills, text)
