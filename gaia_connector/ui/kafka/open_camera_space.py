49716c15735e338c68f44aedc0875ae3d5bd62a2
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-14 14:57:49 +0700

    upgrade env reader

--- gaia_connector/ui/kafka/open_camera_space.py
+++ gaia_connector/ui/kafka/open_camera_space.py
@@ -2,10 +2,10 @@ from ui import bus
 from core.domain.constants import Constants
 
 
-@bus.handle(Constants.KafkaTopic.OPEN_CAMERA_SPACE_TOPIC)
+@bus.handle(Constants.KafkaTopic.OPEN_CAMERA_SPACE)
 def handle_open_camera_space(consumer, msg):
     print(f"Consumed event from topic {msg.topic()}: key = {msg.key()} value = {msg.value()}")
 
-@bus.handle(Constants.KafkaTopic.CLOSE_CAMERA_SPACE_TOPIC)
+@bus.handle(Constants.KafkaTopic.CLOSE_CAMERA_SPACE)
 def handle_close_camera_space(consumer, msg):
     print(f"Consumed event from topic {msg.topic()}: key = {msg.key()} value = {msg.value()}")
