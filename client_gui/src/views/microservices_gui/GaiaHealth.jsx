812e43e3ec3f58a838a856adb29b81719f743f50
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-14 19:58:59 +0700

    add gaia health screen

--- client_gui/src/views/microservices_gui/GaiaHealth.jsx
+++ client_gui/src/views/microservices_gui/GaiaHealth.jsx
@@ -1,7 +1,19 @@
-export const GaiaHealth = () => {
+import ManagerTemplate from '../../components/template/ManagerTemplate';
+
+function ContentArea() {
     return (
         <>
             <h1>GaiaHealth</h1>
         </>
     )
 }
+
+const GaiaHealth = () => {
+    return (
+        <ManagerTemplate>
+            <ContentArea />
+        </ManagerTemplate>
+    )
+}
+
+export default GaiaHealth;
