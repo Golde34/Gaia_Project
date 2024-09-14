3d78e05af24bc656cdb2f21ddbe1567489ccdf27
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-14 12:50:57 +0700

    add vector database

--- gaia_connector/core/domain/constants.py
+++ gaia_connector/core/domain/constants.py
@@ -1,8 +1,10 @@
 class Constants:
     class KafkaTopic:
         CREATE_TASK_TOPIC = "task-manager.create-task.topic"
+        OPEN_CAMERA_SPACE = "OPEN_CAMERA_SPACE"
 
     class StringConstants:
+        kafka_topic_prefix = "KAFKA_TOPICS."
         status = "status"
         response = "response" 
         message = "message"
--- gaia_connector/kernel/configs/load_env.py
+++ gaia_connector/kernel/configs/load_env.py
@@ -1,8 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-from kernel.configs.kafka_config import KAFKA_TOPICS  
-
 
 load_dotenv()
 
@@ -10,9 +8,3 @@ def load_kafka_env():
     kafka_consumer_group = os.getenv("KAFKA_CONSUMER_GROUP")
     kafka_bootstrap_servers = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
     return kafka_consumer_group, kafka_bootstrap_servers
-
-def load_kakfka_topic(service_name: str):
-    topics = []
-    for item in KAFKA_TOPICS[service_name]['kafka_topics']:
-        topics.append(os.getenv(item))
-    return topics
