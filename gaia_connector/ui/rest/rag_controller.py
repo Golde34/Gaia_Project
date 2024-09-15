822561616fd39e30fa2967e34358b565637e44d4
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-15 22:52:57 +0700

    use usecase package in task manager controller

--- gaia_connector/ui/rest/rag_controller.py
+++ gaia_connector/ui/rest/rag_controller.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 
 from ui import app
-from core.services.usecase.rag_file_usecase import RAGFileUsecase
+from gaia_connector.core.usecases.rag_file_usecase import RAGFileUsecase
 
 @app.route('/rag-file/create', methods=['POST'])
 def create_rag_file():
