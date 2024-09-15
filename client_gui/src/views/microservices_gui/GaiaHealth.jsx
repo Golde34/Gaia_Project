68364ed7bdca47b13ba0f588751f0f854a7562df
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-15 12:43:09 +0700

    upgrade tremor

--- client_gui/src/views/microservices_gui/GaiaHealth.jsx
+++ client_gui/src/views/microservices_gui/GaiaHealth.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useState } from 'react';
 import ManagerTemplate from '../../components/template/ManagerTemplate';
+import { Button, Card, Flex, Grid, Metric, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, TextInput, Title } from '@tremor/react';
 
 function ContentArea() {
     const [selectedFile, setSelectedFile] = useState(null);
@@ -14,7 +17,7 @@ function ContentArea() {
     // Function to handle file upload
     const handleFileUpload = async (event) => {
         event.preventDefault();
-        
+
         if (!selectedFile) {
             alert("Please select a file to upload.");
             return;
@@ -42,21 +45,70 @@ function ContentArea() {
     };
 
     return (
-        <div className="mt-6">
-            <form onSubmit={handleFileUpload}>
-                <label className="block mb-2 text-sm font-medium text-gray-700">
-                    Upload File
-                </label>
-                <input
-                    type="file"
-                    onChange={handleFileChange}
-                    className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-                />
-                <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                    Upload
-                </button>
-            </form>
-        </div>
+        <>
+            <Metric style={{ marginBottom: '30px', marginTop: '30px' }}
+                className='text-2xl font-bold text-gray-800'>Gaia Health</Metric>
+            <div className="grid md:grid-cols-5 grid-cols-1 w-full">
+                <div className="col-span-3">
+                    <div className="w-full flex flex-col justify-between p-2">
+                        <div className="flex-auto w-full">
+                            <Card>
+                                <Flex>
+                                    <Title>Upload RAG File</Title>
+                                </Flex>
+                                <form className='pt-5' onSubmit={handleFileUpload}>
+                                    <Flex>
+                                        <TextInput
+                                            id="rag-file"
+                                            type="file"
+                                            onChange={handleFileChange}
+                                            className="mt-4 me-3 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
+                                            accept=".csv, .json, .txt, .pdf"
+                                        />
+                                        <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                                            Upload
+                                        </button>
+                                    </Flex>
+                                    <Title className='mt-2 text-xs text-gray-500 dark:text-gray-500'>
+                                        You are only allowed to upload files with the following extensions: .csv, .json, .txt, .pdf
+                                    </Title>
+                                </form>
+                            </Card>
+                            <Card className='mt-5 flex justify-center'>
+                                <Button variant="primary" color="indigo" className="p-2 rounded-lg mb-4">Add all to VectorDB </Button>
+                                <Button variant="primary" color="rose" className="p-2 rounded-lg ms-4 mb-4"> Delete all from VectorDB </Button>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-2 w-full">
+                    <div className='w-full p-2'>
+                        <Card className='max-w-full mx-auto'>
+                            <Title>List files</Title>
+                            <Table className='mt-5'>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableHeaderCell>File Name</TableHeaderCell>
+                                        <TableHeaderCell>Created Date</TableHeaderCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>File1</TableCell>
+                                        <TableCell>2024-01-01</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>File2</TableCell>
+                                        <TableCell>2024-01-01</TableCell>
+                                    </TableRow>
+
+                                </TableBody>
+                            </Table>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
