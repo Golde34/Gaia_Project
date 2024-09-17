2eb2d77e0661dbc8077a1d2c3d1881da8ff190a2
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-17 23:03:02 +0700

    rag file handler flow

---
+++ gaia_connector/core/services/receiver/rag_service.py
@@ -0,0 +1,38 @@
+from werkzeug.datastructures import FileStorage
+import uuid
+import os
+
+from kernel.utils.file_handler import compute_file_hash, get_file_size
+from core.domain.entities.rag_file import RAGFile 
+
+
+class RagFileHandlerService:
+    def __init__(self) -> None:
+        pass
+
+    def store_rag_file_in_local(file: FileStorage) -> RAGFile:
+        file_id = str(uuid.uuid4())
+        file_hash = compute_file_hash(file)
+        
+        file_name = file.filename
+        file_type = file.content_type
+        file_size = get_file_size(file)
+        
+        file_path = os.path.join('local_storage', f"{file_id}_{file_name}")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
+        file.save(file_path)
+
+        rag_file = RAGFile(
+            file_id=file_id,
+            file_name=file_name,
+            file_path=file_path,
+            file_type=file_type,
+            file_size=file_size,
+            file_hash=file_hash
+        )
+
+        return rag_file 
+
+    def save_rag_file_to_db(rag_file: RAGFile):
+        pass
--- gaia_connector/ui/rest/rag_controller.py
+++ gaia_connector/ui/rest/rag_controller.py
@@ -8,33 +8,7 @@ from core.usecases.rag_file_usecase import RAGFileUsecase
 def create_rag_file():
     print(request.files)
     file = request.files['file']
-    return RAGFileUsecase().upload_rag_file(file)
-
-# def upload_rag_file():
-#     # Debugging: Check what Flask is receiving
-#     print(f"Request Content-Type: {request.content_type}")
-#     print(f"Request files: {request.files}")
-
-#     # Check if the file is in the request
-#     if 'file' not in request.files:
-#         return jsonify({'status': 'ERROR', 'message': 'No file part in the request'}), 400
-
-#     file = request.files['file']
-
-#     # Additional check to see if the filename is empty
-#     if file.filename == '':
-#         return jsonify({'status': 'ERROR', 'message': 'No selected file'}), 400
-
-#     # Save the file
-#     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#     file.save(file_path)
-
-#     return jsonify({
-#         'status': 'OK',
-#         'message': 'File uploaded successfully',
-#         'file_path': file_path
-#     }), 200
-
+    return RAGFileUsecase().create_rag_file(file)
 
 @app.route('/rag-file/update', methods=['PUT'])
 def update_rag_file():
