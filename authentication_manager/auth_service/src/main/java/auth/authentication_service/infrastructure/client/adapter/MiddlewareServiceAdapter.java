9471c0b2710085da6fad5e818bbeb4da5b7c0ac8
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-24 00:01:25 +0700

    finish insert status api

--- authentication_manager/auth_service/src/main/java/auth/authentication_service/infrastructure/client/adapter/MiddlewareServiceAdapter.java
+++ authentication_manager/auth_service/src/main/java/auth/authentication_service/infrastructure/client/adapter/MiddlewareServiceAdapter.java
@@ -1,18 +1,15 @@
 package auth.authentication_service.infrastructure.client.adapter;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import auth.authentication_service.core.domain.dto.GeneralResponse;
 import auth.authentication_service.core.domain.dto.request.ServiceStatusRequest;
 import auth.authentication_service.core.exceptions.BusinessException;
 import auth.authentication_service.core.port.client.MiddlewareServiceClient;
 import auth.authentication_service.infrastructure.client.ClientTemplate;
 import auth.authentication_service.kernel.utils.ClientUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
@@ -34,10 +31,9 @@ public class MiddlewareServiceAdapter implements MiddlewareServiceClient {
         try {
             HttpHeaders requestHeaders = clientUtils.buildDefaultHeaders();
             log.info("Calling API to middleware loader: {}", statusRegister);
-            ResponseEntity<GeneralResponse<String>> response = clientTemplate.post(statusRegister, requestHeaders, request,
-                    new ParameterizedTypeReference<GeneralResponse<String>>() {});
+            ResponseEntity<String> response = clientTemplate.post(statusRegister, requestHeaders, request, String.class);
             log.info("Response from middleware loader: {}", response);
-            return response.getBody().getErrorMessage();
+            return response.getBody();
         } catch (Exception e) {
             throw new BusinessException(String.format("Error when calling API to middleware loader: %s", e.getMessage()));
         }
