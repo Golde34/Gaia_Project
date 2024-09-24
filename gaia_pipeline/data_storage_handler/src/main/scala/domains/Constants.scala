b61a011a45ca53b1d4b15a09378217648f2c3792
Golde34 <nguyendongducviet2001@gmail.com>
2024-09-24 22:29:22 +0700

    fix handle upload file flow

--- gaia_pipeline/data_storage_handler/src/main/scala/domains/Constants.scala
+++ gaia_pipeline/data_storage_handler/src/main/scala/domains/Constants.scala
@@ -4,11 +4,13 @@ object Constants {
     
     object KafkaTopic {
         final val UPLOAD_RAG_FILE: String = "gaia.upload-rag-file.topic"
+        final val PIPELINE_UPLOAD_RAG_FILE: String = "pipeline.upload-rag-file.topic"
     }
 
     object KafkaCmd {
         final val GAIA_INIT_UPLOAD_FILE: String = "gaiaInitUploadFile"
+        final val GAIA_FAILED_UPLOAD_FILE: String = "gaiaFailedUploadFile"
 
-        final val GAIA_UPLOAD_FILE: String = "gaiaUploadFile"
+        final val PIPELINE_INIT_UPLOAD_FILE_RESULT: String = "pipelineInitUploadFileResult"
     }
 }
