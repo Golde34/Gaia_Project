e643698680e36dd9c64e4fdec3da2fce1a43eae1
Golde34 <nguyendongducviet2001@gmail.com>
2024-10-03 07:48:42 +0700

    fix create task function gaia create task flow in GC

--- gaia_connector/core/usecases/task_usecase.py
+++ gaia_connector/core/usecases/task_usecase.py
@@ -8,6 +8,7 @@ from core.services.mapper.task_mapper import TaskMapper
 class TaskUseCase(TaskServiceRequest):
     def __init__(self, url):
         self.url = url
+        self.service = TaskServiceRequest(url)
 
     def create_task(self, data):
         """
@@ -22,7 +23,7 @@ class TaskUseCase(TaskServiceRequest):
             final_task_obj = self.map_group_task_in_task_object(data, task)
             if final_task_obj is None:
                 return jsonify({Constants.StringConstants.status: 'ERROR', Constants.StringConstants.message: 'Invalid group task'})
-            return self.create_task(final_task_obj)
+            return self.service.create_task(final_task_obj)
         except Exception as e:
             print('Create task failed')
             return jsonify({Constants.StringConstants.status: 'ERROR', Constants.StringConstants.message: 'Invalid data'})
