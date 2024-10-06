a8238b9f860ffa1de89659fd6177c72eb94bf109
Golde34 <nguyendongducviet2001@gmail.com>
2024-10-06 11:38:51 +0700

    add task manager adapter in TM

---
+++ work_optimization/src/main/java/wo/work_optimization/infrastructure/client/adapter/TaskmanagerServiceAdapter.java
@@ -0,0 +1,39 @@
+package wo.work_optimization.infrastructure.client.adapter;
+
+import org.apache.kafka.clients.ClientUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import wo.work_optimization.core.domain.dto.response.base.GeneralResponse;
+import wo.work_optimization.core.domain.entity.ParentTask;
+import wo.work_optimization.infrastructure.client.ClientTemplate;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TaskmanagerServiceAdapter {
+    
+    @Value("${app.service.taskmanager-service.api.get-task}")
+    private String getGroupTaskAPI;
+
+    private final ClientTemplate clientTemplate;
+    // private final ClientUtils clientUtils;
+
+    public ParentTask getGroupTask(Long taskId) {
+        try {
+            String uri = String.format(getGroupTaskAPI, taskId);
+            log.info("Calling api to taskmanager service: {}", uri);
+            ResponseEntity<GeneralResponse<ParentTask>> response =  clientTemplate.get(uri, null, 
+                new ParameterizedTypeReference<GeneralResponse<ParentTask>>() {});
+            log.info("Response from taskmanager service: {}", response);
+            return response.getBody().getData();
+        } catch (Exception e) {
+            log.error("Error when call api to taskmanager service: {}", e.getMessage());
+        }
+        return null;
+    }
+}
