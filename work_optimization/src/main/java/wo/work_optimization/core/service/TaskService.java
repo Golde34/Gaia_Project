359883aa60ff71ff483b52123fffb794a181a249
Golde34 <nguyendongducviet2001@gmail.com>
2024-10-15 20:47:13 +0700

    fix bug produce kafka sync task message

--- work_optimization/src/main/java/wo/work_optimization/core/service/TaskService.java
+++ work_optimization/src/main/java/wo/work_optimization/core/service/TaskService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import wo.work_optimization.core.domain.constant.TopicConstants;
 import wo.work_optimization.core.domain.entity.Task;
 import wo.work_optimization.core.domain.kafka.SchedulePlanSyncronizedMessage;
+import wo.work_optimization.core.domain.kafka.base.KafkaBaseDto;
 import wo.work_optimization.core.port.client.SchedulePlanClient;
 import wo.work_optimization.core.port.store.TaskStore;
 import wo.work_optimization.kernel.utils.DataUtils;
@@ -34,17 +35,18 @@ public class TaskService {
 
     public void sendKafkaToSyncWithSchedulePlan(Task task, String errorCode, String errorMessage) {
         log.info("Task before send kafka to sync with schedule plan: {}", task);
-        SchedulePlanSyncronizedMessage message = new SchedulePlanSyncronizedMessage();
+        SchedulePlanSyncronizedMessage data = new SchedulePlanSyncronizedMessage();
         if (dataUtils.isNullOrEmpty(task)) {
-            message = SchedulePlanSyncronizedMessage.builder().taskSynchronizeStatus("Sync fail").build();
+            data = SchedulePlanSyncronizedMessage.builder().taskSynchronizeStatus("Sync fail").build();
         }
-        message = SchedulePlanSyncronizedMessage.builder()
+        data = SchedulePlanSyncronizedMessage.builder()
                 .taskSynchronizeStatus("Sync successfully")
                 .scheduleTaskId(task.getScheduleTaskId())
                 .taskId(task.getOriginalId())
                 .build();
 
-        kafkaPublisher.pushAsync(message, TopicConstants.CreateScheduleTaskCommand.TOPIC, "wo", null);
+        KafkaBaseDto<SchedulePlanSyncronizedMessage> message = data.toKafkaBaseSto(errorCode, errorMessage);
+        kafkaPublisher.pushAsync(message, TopicConstants.CreateScheduleTaskCommand.SYNC_TOPIC, "wo", null);
         log.info("Sent kafka to sync with schedule plan");
     }
 }
