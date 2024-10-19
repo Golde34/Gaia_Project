24c706e3d78ef87200ba07a0ab745b6df7309dde
Golde34 <nguyendongducviet2001@gmail.com>
2024-10-19 21:55:49 +0700

    finish create note

--- person_task_manager/server/src/core/port/mapper/note.mapper.ts
+++ person_task_manager/server/src/core/port/mapper/note.mapper.ts
@@ -12,5 +12,5 @@ export const createNoteMapper = (note: INoteEntity) => {
 
 const convertNewDateToName = () => {
     const date = new Date();
-    return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
