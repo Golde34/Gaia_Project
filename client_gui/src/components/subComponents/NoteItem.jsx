170a8b2b25e3f59bfb5c12798e0c2b2e6486b785
Golde34 <nguyendongducviet2001@gmail.com>
2024-10-20 12:55:20 +0700

    finish note dashboard

--- client_gui/src/components/subComponents/NoteItem.jsx
+++ client_gui/src/components/subComponents/NoteItem.jsx
@@ -6,6 +6,7 @@ import { LockClosedIcon } from "@heroicons/react/solid";
 const NoteText = (props) => {
     return (
         <Metric>
+            <p className="text-sm text-gray-600">{props.displayText}</p>
             <p className="text-sm text-gray-500 line-clamp-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
@@ -15,10 +16,10 @@ const NoteText = (props) => {
                 Class aptent taciti sociosqu ad litora torquent per conubia nostra,
                 per inceptos himenaeos. Curabitur sodales ligula in libero.
             </p>
-                <Button variant="primary" className="p-2" color="indigo"
-                    type="button" onClick={() => navigate(props.url)}
-                > {props.buttonText}
-                </Button>
+            <Button variant="primary" className="p-2" color="indigo"
+                type="button" onClick={() => navigate(props.url)}
+            > {props.buttonText}
+            </Button>
         </Metric>
     )
 }
@@ -33,20 +34,20 @@ const NoteItem = (props) => {
         <Card className="w-xs h-72 hover:cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300"
             decoration="left" decorationColor="indigo">
             <Flex justifyContent="between" alignItems="center">
-                <Metric>{props.name}</Metric>
-                <EllipsisMenu elementName="Note" elementId={props.elementId} />
+                <Metric>{props.note.name}</Metric>
+                <EllipsisMenu elementName="Note" elementId={props.note.id} />
             </Flex>
 
-            {props.isLock ? (
+            {props.note.isLock ? (
                 <Flex justifyContent="center" alignItems="center" className="w-full h-48" >
                     <button className="border-none bg-transparent focus:outline-none"
                         onClick={click}>
-                    <LockClosedIcon className="h-6 w-12 text-gray-500" />
+                        <LockClosedIcon className="h-6 w-12 text-gray-500" />
                     </button>
                 </Flex>
             ) : (
                 <Flex className="mt-4" justifyContent="end">
-                    <NoteText buttonText="OK" url="OK"/>
+                    <NoteText buttonText="OK" url="OK" displayText={props.note.summaryDisplayText} />
                 </Flex>
             )}
         </Card>
