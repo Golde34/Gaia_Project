e1431771ca5e404706f078175ced06e357a8e722
Golde34 <nguyendongducviet2001@gmail.com>
2024-11-16 10:22:16 +0700

    add task optimization router in ML

--- middleware_loader/ui/controller_services/work_optimization/task_optimization_controller.go
+++ middleware_loader/ui/controller_services/work_optimization/task_optimization_controller.go
@@ -1,6 +1,8 @@
 package controller_services
 
 import (
+	"encoding/json"
+	"log"
 	services "middleware_loader/core/services/work_optimization"
 	"net/http"
 
@@ -9,5 +11,15 @@ import (
 
 func OptimizeTaskByUser(w http.ResponseWriter, r *http.Request, taskOptimizationService *services.TaskOptimizationService) {
 	userId := chi.URLParam(r, "userId")
-	taskOptimizationService.OptimizeTaskByUser(userId)
+	result, err := taskOptimizationService.OptimizeTaskByUser(userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		log.Printf("Error encoding final response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
