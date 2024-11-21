75ab8f77570ddccedfcd9f4d20dbb3fb3a35b1b8
Golde34 <nguyendongducviet2001@gmail.com>
2024-11-21 21:44:49 +0700

    add get task list strategy

--- work_optimization/src/main/java/wo/work_optimization/ui/restful/router/TaskOptimizationRouter.java
+++ work_optimization/src/main/java/wo/work_optimization/ui/restful/router/TaskOptimizationRouter.java
@@ -1,15 +1,16 @@
 package wo.work_optimization.ui.restful.router;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import wo.work_optimization.core.domain.dto.request.OptimizeTaskRestRequestDTO;
 import wo.work_optimization.core.domain.dto.response.base.GeneralResponse;
 
 @RequestMapping("/${spring.application.url-name}")
 public interface TaskOptimizationRouter {
 
-    @GetMapping("/optimize-task-by-user")
-    ResponseEntity<GeneralResponse<String>> optimizeTaskByUser(@RequestParam Long userId);
+    @PostMapping("/optimize-task-by-user")
+    ResponseEntity<GeneralResponse<String>> optimizeTaskByUser(@RequestBody OptimizeTaskRestRequestDTO request);
 }
