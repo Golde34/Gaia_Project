184bd3e761818901cc10951c3b0a4954c40f88c4
Golde34 <nguyendongducviet2001@gmail.com>
2024-11-25 23:30:11 +0700

    add generic response in optimize task by user api

--- work_optimization/src/main/java/wo/work_optimization/ui/restful/router/TaskOptimizationRouter.java
+++ work_optimization/src/main/java/wo/work_optimization/ui/restful/router/TaskOptimizationRouter.java
@@ -1,7 +1,5 @@
 package wo.work_optimization.ui.restful.router;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import wo.work_optimization.core.domain.dto.request.OptimizeTaskRestRequestDTO;
 import wo.work_optimization.core.domain.dto.response.base.GeneralResponse;
-import wo.work_optimization.core.domain.entity.Task;
 
 @RequestMapping("/${spring.application.url-name}")
 public interface TaskOptimizationRouter {
 
     @PostMapping("/optimize-task-by-user")
-    ResponseEntity<GeneralResponse<List<Task>>> optimizeTaskByUser(@RequestBody OptimizeTaskRestRequestDTO request);
+    ResponseEntity<GeneralResponse<?>> optimizeTaskByUser(@RequestBody OptimizeTaskRestRequestDTO request);
 }
