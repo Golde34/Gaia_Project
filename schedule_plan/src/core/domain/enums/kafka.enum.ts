bb8a78ee28414d9a951ed23c7f69965bbb3abda6
Golde34 <nguyendongducviet2001@gmail.com>
2024-12-05 20:46:57 +0700

    add register schedule plan api

--- schedule_plan/src/core/domain/enums/kafka.enum.ts
+++ schedule_plan/src/core/domain/enums/kafka.enum.ts
@@ -15,7 +15,6 @@ export enum KafkaCommand {
     SYNC_SCHEDULE_TASK = "syncScheduleTask",
     OPTIMIZE_SCHEDULE_TASK = "optimizeScheduleTask",
     OPTIMIZE_TASK = "optimizeTask",
-    REGISTER_SCHEDULE_PLAN = "registerSchedulePlan",
 }
 
 export class KafkaMessage {
