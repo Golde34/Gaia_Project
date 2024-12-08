620dba9dbe15bcad6a936685200403d405e51def
Golde34 <nguyendongducviet2001@gmail.com>
2024-12-08 10:55:36 +0700

    fix priority of SP

--- middleware_loader/core/domain/dtos/response/schedule_task_dto.go
+++ middleware_loader/core/domain/dtos/response/schedule_task_dto.go
@@ -1,25 +1,24 @@
 package response_dtos
 
 type ScheduleTaskResponseDTO struct {
-	ID           string  `json:"id"`
-	Title        string  `json:"title"`
-	Priority     float64 `json:"priority"`
-	Status       string  `json:"status"`
-	StartDate    string  `json:"startDate"`
-	Deadline     string  `json:"deadline"`
-	Duration     float64 `json:"duration"`
-	ActiveStatus string  `json:"activeStatus"`
-	PreferenceLevel float64 `json:"preferenceLevel"`
-	TaskId       string  `json:"taskId"`
-	IsSynchronizedWithWO bool `json:"isSynchronizedWithWO"`
-	SchedulePlanId string `json:"schedulePlanId"`
-	StopTime float64 `json:"stopTime"`
-	TaskBatch float64 `json:"taskBatch"`
-	TaskOrder float64 `json:"taskOrder"`
-	Weight float64 `json:"weight"`
+	ID                   string   `json:"id"`
+	Title                string   `json:"title"`
+	Priority             []string `json:"priority"`
+	Status               string   `json:"status"`
+	StartDate            string   `json:"startDate"`
+	Deadline             string   `json:"deadline"`
+	Duration             float64  `json:"duration"`
+	ActiveStatus         string   `json:"activeStatus"`
+	PreferenceLevel      float64  `json:"preferenceLevel"`
+	TaskId               string   `json:"taskId"`
+	IsSynchronizedWithWO bool     `json:"isSynchronizedWithWO"`
+	SchedulePlanId       string   `json:"schedulePlanId"`
+	StopTime             float64  `json:"stopTime"`
+	TaskBatch            float64  `json:"taskBatch"`
+	TaskOrder            float64  `json:"taskOrder"`
+	Weight               float64  `json:"weight"`
 }
 
 func NewScheduleTaskResponseDTO() *ScheduleTaskResponseDTO {
 	return &ScheduleTaskResponseDTO{}
 }
-
--- middleware_loader/core/port/mapper/response/schedule_task_response.go
+++ middleware_loader/core/port/mapper/response/schedule_task_response.go
@@ -1,42 +1,39 @@
 package mapper
 
-import response_dtos "middleware_loader/core/domain/dtos/response"
+import (
+	response_dtos "middleware_loader/core/domain/dtos/response"
+	"middleware_loader/kernel/utils"
+)
 
 func ReturnScheduleTaskObjectMapper(body map[string]interface{}) *response_dtos.ScheduleTaskResponseDTO {
 	var input response_dtos.ScheduleTaskResponseDTO
 	input.ID = body["_id"].(string)
-	if (body["title"] != nil) {
-		input.Title = body["title"].(string)
+	input.Title = body["title"].(string)
+	input.Priority = utils.ConvertStringToStringArray(body["priority"].([]interface{}))
+	input.Status = body["status"].(string)
+	input.StartDate = body["startDate"].(string)
+	input.Deadline = body["deadline"].(string)
+	input.Duration = body["duration"].(float64)
+	input.ActiveStatus = body["activeStatus"].(string)
+	input.PreferenceLevel = body["preferenceLevel"].(float64)
+	input.TaskId = body["taskId"].(string)
+	if body["isSynchronizedWithWO"] != nil {
+		input.IsSynchronizedWithWO = body["isSynchronizedWithWO"].(bool)
 	}
-	if (body["priority"] != nil) {
-		input.Priority = body["priority"].(float64)
+	if body["taskOrder"] != nil {
+		input.TaskOrder = body["taskOrder"].(float64)
 	}
-	if (body["status"] != nil) {
-		input.Status = body["status"].(string)
+	if body["weight"] != nil {
+		input.Weight = body["weight"].(float64)
 	}
-	if (body["startDate"] != nil) {
-		input.StartDate = body["startDate"].(string)
+	if body["stopTime"] != nil {
+		input.StopTime = body["stopTime"].(float64)
 	}
-	if (body["deadline"] != nil) {
-		input.Deadline = body["deadline"].(string)
+	if body["taskBatch"] != nil {
+		input.TaskBatch = body["taskBatch"].(float64)
 	}
-	if (body["duration"] != nil) {
-		input.Duration = body["duration"].(float64)
+	if body["schedulePlanId"] != nil {
+		input.SchedulePlanId = body["schedulePlanId"].(string)
 	}
-	if (body["activeStatus"] != nil) {
-		input.ActiveStatus = body["activeStatus"].(string)
-	}
-	if (body["preferenceLevel"] != nil) {
-		input.PreferenceLevel = body["preferenceLevel"].(float64)
-	}
-	if (body["taskId"] != nil) {
-		input.TaskId = body["taskId"].(string)
-	}
-	input.IsSynchronizedWithWO = body["isSynchronizedWithWO"].(bool)
-	input.TaskOrder = body["taskOrder"].(float64)
-	input.Weight = body["weight"].(float64)
-	input.StopTime = body["stopTime"].(float64)
-	input.TaskBatch = body["taskBatch"].(float64)
-	input.SchedulePlanId = body["schedulePlanId"].(string)
 	return &input
 }
