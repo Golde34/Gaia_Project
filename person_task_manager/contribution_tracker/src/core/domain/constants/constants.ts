b7f2e9bf9876b6dae4a81d39902362f6c569e373
Golde34 <nguyendongducviet2001@gmail.com>
2025-01-12 22:22:40 +0700

    add cache wehn query user info

--- person_task_manager/contribution_tracker/src/core/domain/constants/constants.ts
+++ person_task_manager/contribution_tracker/src/core/domain/constants/constants.ts
@@ -8,11 +8,8 @@ export const NOT_EXISTED_MESSAGE = "%s is not existed";
 export const EMPTY_SENTENCE = "This task has no sentence";
 
 export class InternalCacheConstants {
-    public static readonly CACHE_PREFIX = "task-manager.";
+    public static readonly CACHE_PREFIX = "contribution-tracker.";
     public static readonly CACHE_POSTFIX = ".cache";
 
-    public static TASK_TABLE = `task-table.`;
-    public static TASK_COMPLETED = `task-completed.`;
-    public static NOTE_LIST = `note-list.`;
-    public static NOTE_DETAIL = `note-detail.`;
+    public static USER_INFO_CACHE_KEY = "user-info";
 }
--- person_task_manager/contribution_tracker/src/core/service/user-commit.service.ts
+++ person_task_manager/contribution_tracker/src/core/service/user-commit.service.ts
@@ -1,18 +1,30 @@
+import CacheSingleton from "../../infrastructure/cache/cache-singleton";
 import UserCommitRepository from "../../infrastructure/repository/user-commit.repository";
+import { InternalCacheConstants } from "../domain/constants/constants";
 
 class UserCommitService {
     constructor(
-        private userCommitRepository: UserCommitRepository = UserCommitRepository.getInstance() 
+        private userCommitRepository: UserCommitRepository = UserCommitRepository.getInstance(),
+        private userCommitCache = CacheSingleton.getInstance().getCache()
     ) {}
 
     async getUserGithubInfo(userId: number): Promise<any> {
         try {
+            const cachedUserGithubInfo = this.userCommitCache.get(InternalCacheConstants.USER_INFO_CACHE_KEY + userId);
+            if (cachedUserGithubInfo) {
+                return cachedUserGithubInfo;
+            }
             const userGithubInfo = await this.userCommitRepository.findByUserId(userId);
+            this.userCommitCache.set(InternalCacheConstants.USER_INFO_CACHE_KEY + userId, userGithubInfo);
             return userGithubInfo; 
         } catch (error) {
             console.error("Error on getUserGithubInfo: ", error);
             return null;
         }
+    }
+
+    async clearUserCache(userId: number): Promise<void> {
+        this.userCommitCache.clear(InternalCacheConstants.USER_INFO_CACHE_KEY + userId);
     }
 }
 
