c8a2cd5dd67910652bf3483389931bcbafd5fddf
Golde34 <nguyendongducviet2001@gmail.com>
2025-01-15 17:35:07 +0700

    add ct service configuration repository

---
+++ person_task_manager/contribution_tracker/src/core/domain/entities/ct-service-configuration.entity.ts
@@ -0,0 +1,9 @@
+export interface CTServiceConfigurationEntity {
+    id: number;
+    paramType: string;
+    paramName: string;
+    paramValue: string;
+    description: string;
+    entity: string;
+    status: boolean;
+}
---
+++ person_task_manager/contribution_tracker/src/infrastructure/repository/ct-service-config.repository.ts
@@ -0,0 +1,21 @@
+import { CTServiceConfigurationEntity } from "../../core/domain/entities/ct-service-configuration.entity";
+import Repository from "../database/repository";
+
+export class CTServiceConfigRepository extends Repository {
+    private static instance: CTServiceConfigRepository;
+
+    public static getInstance(): CTServiceConfigRepository {
+        if (!CTServiceConfigRepository.instance) {
+            CTServiceConfigRepository.instance = new CTServiceConfigRepository();
+        }
+        return CTServiceConfigRepository.instance;
+    }
+
+    constructor() {
+        super('ct_service_configuration');
+    }
+
+    async findConfigByParamType(paramType: string): Promise<CTServiceConfigurationEntity[]> {
+        return await this.findByCondition('param_type = ? and status = ?', [paramType, true]);
+    }
+}
