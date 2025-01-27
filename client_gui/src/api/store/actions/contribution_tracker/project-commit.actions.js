0fdd688f3f89df0c955c84209ac687e3e8abf8bf
Golde34 <nguyendongducviet2001@gmail.com>
2025-01-27 11:51:51 +0700

    add sync project repo api

--- client_gui/src/api/store/actions/contribution_tracker/project-commit.actions.js
+++ client_gui/src/api/store/actions/contribution_tracker/project-commit.actions.js
@@ -23,7 +23,12 @@ export const getProjectsAndRepos = (userId) => async (dispatch) => {
 export const syncProjectAndRepo = (userId, project, repo) => async (dispatch) => {
     dispatch({ type: SYNC_PROJECT_AND_REPO_REQUEST, payload: userId });
     try {
-        const { data } = await serverRequest(`/project-commit/user-github/sync-project-repo/${userId}/${project}/${repo}`, HttpMethods.POST, portName.middlewarePort);
+        const body = {
+            userId,
+            project,
+            repo
+        }
+        const { data } = await serverRequest(`/project-commit/sync-project-repo`, HttpMethods.POST, portName.middlewarePort, body);
         dispatch({ type: SYNC_PROJECT_AND_REPO_SUCCESS, payload: data.data });
     } catch (error) {
         dispatch({
