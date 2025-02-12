cb7c5af0e60c6d1663a46252df2d2605958e804e
Golde34 <nguyendongducviet2001@gmail.com>
2025-02-13 00:40:14 +0700

    create contribution tracker service, no need to symbiosis with TM anymore

---
+++ contribution_tracker/src/test/java/ct/contribution_tracker/ContributionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ct.contribution_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributionTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
